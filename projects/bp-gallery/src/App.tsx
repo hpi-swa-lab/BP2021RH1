d0b6df3412948a18d3bce7f221e501a5ac1a1f02
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-10-13 15:10:10 +0200

    Adjusted Routing mechanism to work with React-Router-Config
    This enables us the to have a app-wide definition of possible routes in one centralized place.
    At the moment this is the routes.tsx file.
    
    Futher reading:
    - https://www.npmjs.com/package/react-router-config/v/5.0.1
    - https://github.com/remix-run/react-router

--- projects/bp-gallery/src/App.tsx
+++ projects/bp-gallery/src/App.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-
-import Home from "./Home";
+import { renderRoutes, RouteConfig } from "react-router-config";
 import Navigation from "./Navigation";
-
-import Demo from './prototypes/demo/Demo';
-
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    <BrowserRouter>
+const App = ({ route }: RouteConfig) => (
+  <div className="App">
     <Navigation />
-      <Switch>
-        <Route path="/prototypes/demo" component={ Demo }/>
-        <Route component={ Home } />
-      </Switch>
-    </BrowserRouter>
-    </div>
-  );
-}
+    { renderRoutes(route.routes) }
+  </div>
+);
 
 export default App;
--- projects/bp-gallery/src/prototypes/demo/index.tsx
+++ projects/bp-gallery/src/prototypes/demo/index.tsx
@@ -1,14 +1,3 @@
-import React from 'react';
-import Demo from './Demo';
+import Demo from "./Demo";
 
-const DemoModule = () => (
-    <div>Demo Module</div>
-);
-
-export default {
-    routeProps: {
-        path: '/prototypes/demo',
-        component: Demo
-    },
-    name: 'Demo',
-}
+export default Demo;
