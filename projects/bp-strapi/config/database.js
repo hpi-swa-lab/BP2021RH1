d403033920ed687e62fdec28877c1442f1104f17
TheGrayStone <lissner.nils@liwe-design.com>
2021-10-27 10:12:55 +0200

    Sync up server code with repo
    Co-authored-by: TheGrayStone <lissner.nils@liwe-design.com>

--- projects/bp-strapi/config/database.js
+++ projects/bp-strapi/config/database.js
@@ -1,15 +1,35 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "bp-strapi"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "abc1234"),
+        schema: env("DATABASE_SCHEMA", "public"),
       },
       options: {
-        useNullAsDefault: true,
-      },
+        debug: false
+      }
     },
   },
 });
---
+++ projects/bp-strapi/plugins/bulk-import/controllers/bulk-import.js
@@ -0,0 +1,312 @@
+'use strict';
+
+/**
+ * bulk-import.js controller
+ *
+ * @description: A set of functions called "actions" of the `bulk-import` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  },
+  import: async (ctx) => {
+    // // strapi.services['keyword-tag'].delete({id: 1});
+    // await strapi.connections.default.raw('DELETE FROM `titles_pictures__pictures_titles`');
+    // ctx.send('ok');
+    // return;
+    // strapi.services.title.delete();
+    // strapi.services.description.delete();
+    // strapi.services['keyword-tag'].delete();
+    // strapi.services['category-tag'].delete();
+    // strapi.services.picture.delete();
+    const fs = require('fs');
+    const data = fs.readFileSync(ctx.request.files['jsondata'].path, 'utf-8');
+    const parsed = JSON.parse(data);
+    const data2 = fs.readFileSync(ctx.request.files['jsondata2'].path, 'utf-8');
+    const tagInfoData = JSON.parse(data2);
+
+    const existingNames = fs.readdirSync('/home/dev/BP2021RH1/projects/bp-strapi/public/uploads/').map(name => name.replace(/_/gm, '-'));
+
+    const buffer = {};
+    // for (const albumid of Object.keys(parsed)) {
+    //   const album = parsed[albumid];
+    //   const allFiles = [];
+    //   for (const picid of Object.keys(album.pictures)) {
+    //     // strapi.services['keyword-tag'].delete({'name': 'Bad Harzburg'});
+    //     const picture = album.pictures[picid];
+    //     const path = `/home/dev/images/gallery/${album.dirname}/${picture.filename}`;
+    //     if (fs.existsSync(path) && !existingNames.some(exName => 
+    //       exName.substring(0, -15) === picture.filename.substring(0, -4)
+    //     )) {
+    //       const stats = fs.statSync(path);
+    //       const file = {
+    //         path,
+    //         name: picture.filename,
+    //         size: stats.size,
+    //         type: 'image/jpeg'
+    //       };
+    //       allFiles.push(file);
+    //       console.log('New image found:' + picture.filename);
+    //     }
+    //   }
+    //   if (allFiles.length > 0) console.log('New images in folder:' + album.dirname)
+    //   await strapi.plugins['upload'].services.upload.upload({data: {
+    //     fileInfo: {}
+    //   }, files: allFiles});
+      
+    //   for (const picid of Object.keys(album.pictures)) {
+    //     const picture = album.pictures[picid];
+    //     const mediaRef = await strapi.plugins['upload'].services.upload.fetch({
+    //       "name": picture.filename
+    //     });
+    //     if (!mediaRef) {
+    //       console.log(picture.filename);
+    //       continue;
+    //     }
+    //     if (mediaRef.related && mediaRef.related.length) {
+    //       continue;
+    //     }
+    //     console.log('Including image', picid);
+    //     let previousTitle = await strapi.services.title.findOne({text: picture.alttext});
+
+    //     if (!previousTitle) {
+    //       previousTitle = await strapi.services.title.create({
+    //         text: picture.alttext
+    //       });
+    //     }
+    //     let previousDescription = null;
+    //     if (picture.description && picture.description !== '') {
+    //       previousDescription = await strapi.services.description.findOne({text: picture.description});
+    //       if (!previousDescription) {
+    //         previousDescription = await strapi.services.description.create({
+    //           text: picture.description
+    //         });
+    //       }
+    //     }
+    //     const keywordRefs = [];
+    //     for (const keyword of picture.keywords) {
+    //       let previousKeyword = await strapi.services['keyword-tag'].findOne({name: keyword});
+    //       if (!previousKeyword) {
+    //         previousKeyword = await strapi.services['keyword-tag'].create({
+    //           name: keyword
+    //         });
+    //       }
+    //       keywordRefs.push({
+    //         id: previousKeyword.id
+    //       });
+    //     }
+    //     const commentRefs = picture.comments.map(comment => {
+    //       return {
+    //         text: comment.content,
+    //         author: comment.author
+    //       }
+    //     });
+    //     buffer[picid] = await strapi.services.picture.create({
+    //       title: {id: previousTitle.id},
+    //       descriptions: (previousDescription ? [{id: previousDescription.id}] : []),
+    //       media: {id: mediaRef.id},
+    //       keyword_tags: keywordRefs,
+    //       comment: commentRefs
+    //     });
+
+    //   }
+    //   // for (const picid of Object.keys(album.pictures)) {
+    //   //   const picture = album.pictures[picid];
+    //   //   const uid = picture.media.id;
+    //   // }
+    // }
+    console.log('Upload finished');
+
+    const parseTagJson = async (cat, tagsSoFar) => {
+      console.log('Parsing category tag', cat.title);
+      let tag = await strapi.services['category-tag'].findOne({name: cat.title});
+      if (!tag) {
+        tag = await strapi.services['category-tag'].create({
+          name: cat.title,
+          description: cat.description,
+          priority: cat.priority
+        });
+      }
+      tagsSoFar = tagsSoFar.concat([{id: tag.id}]);
+      if (!isNaN(parseInt(cat.albumId))) {
+        const newAlbumId = Object
+        .keys(parsed)
+        .find(albumid => parseInt(albumid) === parseInt(cat.albumId));
+        const allPicturesInAlbum = parsed[newAlbumId].pictures;
+
+        for (const picid of Object.keys(allPicturesInAlbum)) {
+          const picture = allPicturesInAlbum[picid];
+          const mediaObject = await strapi.plugins['upload'].services.upload.fetch({name: picture.filename});
+          const imageIdInDatabase = mediaObject.related[0].id;
+          await strapi.services.picture.update({id: imageIdInDatabase}, {
+            category_tags: tagsSoFar
+          });
+        }
+      }
+
+      for (const child of cat.children) {
+        await parseTagJson(child, tagsSoFar);
+      }
+    };
+    await parseTagJson(tagInfoData, []);
+
+    ctx.send(buffer);
+  },
+  checkMissing: async (ctx) => {
+    // ctx.send((await strapi.plugins['upload'].services.upload.fetchAll({_limit: -1}))
+    //   .reduce((p,c) => {p[c.name] = c.related; return p;}, {})
+    // );
+    // ctx.send(await strapi.plugins['upload'].services.upload.fetchAll({
+    //   'name': '02.jpg'
+    // }));
+    const matchAnyTime = (text) => {
+      const yearRegex = /19[Xx0-9]{2}/gm;
+      const yearSpanRegex = /19[Xx0-9]{2}[\s]*[–|-|bis][\s]*19[Xx0-9]{2}/gm;
+      const monthYearMatch = /(?:januar|februar|märz|april|mai|juni|juli|august|september|oktober|november|dezember)[\s]+19[Xx0-9]{2}/gmi;
+      const wholeDateRegex = /([\s]+|^)[\d]{1,2}\.[\s]*(januar|februar|märz|april|mai|juni|juli|august|september|oktober|november|dezember|[\d]{1,2})(?:[\s]+|\.)19[Xx0-9]{2}/gmi;
+      const exactMatch = text.match(wholeDateRegex) || [];
+      const monthMatch = text.match(monthYearMatch) || [];
+      const spanMatch = text.match(yearSpanRegex) || [];
+      const yearMatch = text.replace(yearSpanRegex, '').match(yearRegex) || [];
+      return exactMatch.concat(monthMatch).concat(spanMatch).concat(yearMatch);
+    }
+
+    const parseTimes = (arr) => {
+      const months = ["januar", "februar", "märz", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "dezember"];
+      return arr.map(str => {
+        const parts = str.replace(/[\s]+/gm, '').replace(/(januar|februar|märz|april|mai|juni|juli|august|september|oktober|november|dezember)/gmi, (match) => {
+          return (months.indexOf(match.toLowerCase())+1) + '.';
+        }).split('.');
+        if (parts.length > 2) {
+          parts[2] = parts[2].replace(/x/gim, '0');
+          const start = new Date(`${parts[1]}.${parts[0]}.${parts[2]}`);
+          const end = new Date(start);
+          end.setDate(end.getDate()+1);
+          end.setMilliseconds(end.getMilliseconds()-1);
+          return [start, end];
+        } else if (parts.length > 1) {
+          parts[1] = parts[1].replace(/x/gim, '0');
+          const start = new Date(`${parts[0]}.01.${parts[1]}`);
+          const end = new Date(start);
+          end.setMonth(end.getMonth()+1);
+          end.setDate(end.getDate()-1);
+          return [start, end];
+        } else if (parts[0].match(/[–|-|bis]/gm)) {
+          let [start, end] = parts[0].split(/[\s]*[–|-|bis][\s]*/gm);
+          start = new Date(`${start}`);
+          end = new Date(`${end}`);
+          end.setFullYear(end.getFullYear()+1);
+          end.setDate(end.getDate()-1);
+          return [start, end];
+        } else {
+          if (parts[0][3].toLowerCase() === 'x') {
+            let start, end;
+            if (parts[0][2].toLowerCase() === 'x'){
+              start = new Date(`${parts[0].slice(0,2)}00`);
+              end = new Date(start);
+              end.setFullYear(end.getFullYear()+100);
+              end.setDate(end.getDate()-1);
+            } else {
+              start = new Date(`${parts[0].slice(0,3)}0`);
+              end = new Date(start);
+              end.setFullYear(end.getFullYear()+10);
+              end.setDate(end.getDate()-1);
+            }
+            return [start, end];
+          }
+          const start = new Date(parts[0]);
+          const end = new Date(start);
+          end.setFullYear(end.getFullYear()+1);
+          end.setDate(end.getDate()-1);
+          return [start, end];
+        }
+      });
+    }
+
+    const findTimes = (picture) => {
+      let times = [];
+      if (picture.descriptions) {
+        picture.descriptions.forEach(description => {
+          times = times.concat(matchAnyTime(description.text));
+        });
+      }
+      if (picture.title) {
+        times = times.concat(matchAnyTime(picture.title.text));
+      }
+      picture.category_tags.forEach((tag) => {
+        if (tag.priority < 2) {return; }
+        times = times.concat(matchAnyTime(tag.name));
+        if (tag.description) {
+          times = times.concat(matchAnyTime(tag.description));
+        }
+      });
+      return times;
+    };
+
+    const times = (await strapi.services.picture.find({_limit: -1})).reduce((acc, picture) => {
+      const times = findTimes(picture).filter(t => t);
+      acc[picture.id] = parseTimes(times.filter((t, i) => times.indexOf(t) === i)).sort((a, b) => {
+        return Math.abs(a[1] - a[0]) - Math.abs(b[1] - b[0]);
+      });
+      return acc;
+    }, {});
+    for (const picid of Object.keys(times)) {
+      for (const timerange of times[picid]) {
+        const startDate = new Date(timerange[0]);
+        const endDate = new Date(timerange[1]);
+        let previousTag = await strapi.services['time-range-tag'].findOne({start: startDate, end: endDate});
+        if (!previousTag) {
+          previousTag = await strapi.services['time-range-tag'].create({
+            start: startDate,
+            end: endDate
+          });
+        }
+      }
+      if (times[picid].length > 0) {
+        console.log(picid);
+        const startDate = new Date(times[picid][0][0]);
+        const endDate = new Date(times[picid][0][1]);
+        const mostSpecificTag = await strapi.services['time-range-tag'].findOne({start: startDate, end: endDate});
+        await strapi.services.picture.update({id: picid}, {
+          time_range_tag: {id: mostSpecificTag.id}
+        });
+      }
+    }
+    ctx.send(times);
+    // ctx.send((await strapi.services.picture.find({_limit: 50})));
+  },
+  fillCategoryTags: async (ctx) => {
+    const fs = require('fs');
+    const data = fs.readFileSync(ctx.request.files['jsondata'].path, 'utf-8');
+    const parsed = JSON.parse(data);
+
+    const relateTags = async (tag) => {
+      const relatedTags = await Promise.all(tag.children.map(async child => {
+        return (await strapi.services['category-tag'].findOne({name: child.title})).id;
+      }));
+      console.log(tag.title, relatedTags);
+      // await strapi.services['category-tag'].update({name: tag.title}, {
+      //   related_tags: relatedTags
+      // });
+      for (const child of tag.children) {
+        relateTags(child);
+      }
+    }
+
+    await relateTags(parsed);
+    return {'success': true};
+  }
+};
