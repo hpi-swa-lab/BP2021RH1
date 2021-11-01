f9da097895f4f89679688b742eeeb57066c0819c
TheGrayStone <nilslissner@gmx.de>
2021-11-01 11:42:53 +0100

    Set up environment templates

--- projects/bp-strapi/config/database.js
+++ projects/bp-strapi/config/database.js
@@ -20,12 +20,12 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "bp-strapi"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "abc1234"),
-        schema: env("DATABASE_SCHEMA", "public"),
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        schema: env("DATABASE_SCHEMA"),
       },
       options: {
         debug: false
--- projects/bp-strapi/config/server.js
+++ projects/bp-strapi/config/server.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 9000),
-    url: "https://bp.bad-harzburg-stiftung.de/api/",   
+    host: env('HOST'),
+    port: env.int('PORT'),
+    url: env('URL'),
     proxy: true,
     admin: {
-	url: "https://bp.bad-harzburg-stiftung.de/api/admin",   
-	auth: {
-	    secret: env('ADMIN_JWT_SECRET', '83ed5783afea9091ed95606f08c3c2a0'),
-	},
-	autoOpen: false,
+      url: env('URL') + 'admin',
+      auth: {
+          secret: env('ADMIN_JWT_SECRET'),
+      },
+      autoOpen: false,
     },
 });
