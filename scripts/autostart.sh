6b44c4a0dbe5a9307bab57da802cfcbd32f205fa
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-09 17:30:16 +0100

    Remove dev-version of bp-gallery from autostart
    As we now deploy our bp-gallery in a production version on the server,
    we don't need a separate dev-version on the server anymore.
    
    Instead the new development process will be:
    - Develop bp-gallery locally on a separate branch.
    - Create a PR for your branch to be merged into main.
    - When the PR is merged, the project will be automatically deployed on the server via a GitHub Action.

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,14 +2,9 @@
 
 SESSION_NAME="dev"
 STRAPI_WINDOW_NAME="strapi"
-APP_WINDOW_NAME="app"
 
 echo "Creating Session"
 tmux new-session -A -s $SESSION_NAME -n $STRAPI_WINDOW_NAME -d
 echo "Session created"
 tmux send-keys -t $STRAPI_WINDOW_NAME "cd /home/dev/BP2021RH1/projects/bp-strapi" C-m
 tmux send-keys -t $STRAPI_WINDOW_NAME "yarn develop" C-m
-
-tmux new-window -n $APP_WINDOW_NAME
-tmux send-keys -t $APP_WINDOW_NAME "cd /home/dev/BP2021RH1/projects/bp-gallery" C-m
-tmux send-keys -t $APP_WINDOW_NAME "yarn start" C-m
