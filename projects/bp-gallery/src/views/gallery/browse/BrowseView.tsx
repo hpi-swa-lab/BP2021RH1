9a9b097bf16e4b7990c72df54ed09e4ee2fe1963
Hendrik Woelert <hendrik.woelert@online.de>
2021-11-15 15:10:13 +0100

    linter

--- projects/bp-gallery/src/views/gallery/browse/BrowseView.tsx
+++ projects/bp-gallery/src/views/gallery/browse/BrowseView.tsx
@@ -9,44 +9,46 @@ const BrowseView = (params?: { path?: string[] }) => {
   const [items, setItems] = useState<ItemListItem[]>([]);
 
   useEffect(() => {
-    apiConnector.getCategories(params?.path ?? []).then(async (categories: any[]) => {
-      setItems(
-        await Promise.all(
-          categories.map(async (category, indx) => {
-            const formats = category.thumbnail[0].media.formats;
+    apiConnector
+      .getCategories(params?.path ?? [])
+      .then(async (categories: { thumbnail: any[]; name: string }[]) => {
+        setItems(
+          await Promise.all(
+            categories.map(async (category, indx) => {
+              const formats = category.thumbnail[0].media.formats;
 
-            return {
-              name: decodeURIComponent(category.name),
-              background: `${apiBase}/${String(
-                formats?.medium?.url || formats?.small?.url || formats?.thumbnail?.url || ''
-              )}`,
-              color: indx % 2 === 0 ? '#7E241D' : '#404272',
-              onClick: () => {
-                console.log(params?.path);
-                console.log(
-                  `/browse/${
-                    params?.path
-                      ?.map(folder => {
-                        return encodeURIComponent(folder);
-                      })
-                      .join('/') ?? ''
-                  }/${encodeURIComponent(category.name)}`.replace(/\/+/gm, '/')
-                );
-                history.push(
-                  `/browse/${
-                    params?.path
-                      ?.map(folder => {
-                        return encodeURIComponent(folder);
-                      })
-                      .join('/') ?? ''
-                  }/${encodeURIComponent(category.name)}`.replace(/\/+/gm, '/')
-                );
-              },
-            };
-          })
-        )
-      );
-    });
+              return {
+                name: decodeURIComponent(category.name),
+                background: `${apiBase}/${String(
+                  formats?.medium?.url || formats?.small?.url || formats?.thumbnail?.url || ''
+                )}`,
+                color: indx % 2 === 0 ? '#7E241D' : '#404272',
+                onClick: () => {
+                  console.log(params?.path);
+                  console.log(
+                    `/browse/${
+                      params?.path
+                        ?.map(folder => {
+                          return encodeURIComponent(folder);
+                        })
+                        .join('/') ?? ''
+                    }/${encodeURIComponent(category.name)}`.replace(/\/+/gm, '/')
+                  );
+                  history.push(
+                    `/browse/${
+                      params?.path
+                        ?.map(folder => {
+                          return encodeURIComponent(folder);
+                        })
+                        .join('/') ?? ''
+                    }/${encodeURIComponent(category.name)}`.replace(/\/+/gm, '/')
+                  );
+                },
+              };
+            })
+          )
+        );
+      });
   }, [params?.path, history]);
 
   return (
