d71700e5dd59e93e3ef360557caa8e46eaca3f59
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-21 16:26:37 +0100

    Get rid of a quite meaningless test in app.test.tsx
    The 'Navigation bar is always visible' test is quite 
    meaningless/unlogical for following reasons:
    - it is simply not true as the navbar isn't visible in all routes (e.g. 
    the demo prototypes)
    - in the pictureView and galleryView tests some things of the navbar get 
    queried, so it is unnecessary here
    - and most importantly (together with the first point) the use of 
    'setTimeout' has hidden the fact that the test is actually failing.

--- projects/bp-gallery/src/app.test.tsx
+++ projects/bp-gallery/src/app.test.tsx
@@ -1,25 +1,12 @@
 import routes from './routes';
 import { renderRoute } from './testUtils';
 
-test('Navigation bar is always visible', () => {
-  routes[0]?.routes?.forEach(route => {
-    renderRoute(route.path as string);
-
-    window.setTimeout(() => {
-      const navBars = document.getElementsByClassName('nav-bar');
-      expect(navBars.length).toBe(1);
-    }, 0);
-  });
-});
-
 test('Top bar is always visible', () => {
   routes[0]?.routes?.forEach(route => {
-    renderRoute(route.path as string);
+    const { container } = renderRoute(route.path as string);
 
-    window.setTimeout(() => {
-      const topBars = document.getElementsByClassName('top-bar');
+    const topBars = container.getElementsByClassName('top-bar');
 
-      expect(topBars.length).toBe(1);
-    }, 0);
+    expect(topBars).toHaveLength(1);
   });
 });
