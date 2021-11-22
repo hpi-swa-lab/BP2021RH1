795a477220ea121b2d152061894d4dcc08f8c972
Benedikt Vidic <benedikt.vidic@student.hpi.uni-potsdam.de>
2021-11-22 13:39:50 +0100

    removed unused import

--- projects/bp-gallery/src/views/picture/PictureView.tsx
+++ projects/bp-gallery/src/views/picture/PictureView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './PictureView.scss';
 import PictureDetails from './PictureDetails';
 import CommentsContainer from './comments/CommentsContainer';
