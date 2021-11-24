f191bf9a73e551fa1057f581e1449c8c1b541460
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-24 09:14:53 +0000

    Only allow certain names for category tag names

---
+++ projects/bp-strapi/config/plugins.js
@@ -0,0 +1,15 @@
+/*
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 5000,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+};
+*/
