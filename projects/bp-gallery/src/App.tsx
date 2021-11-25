6f2fd2823771fcba8d1b0e111cf4b6680bcf9d96
Benedikt Vidic <benedikt.vidic@student.hpi.uni-potsdam.de>
2021-11-25 12:01:17 +0100

    Initial configurations for auto-generated APIConnection

--- projects/bp-gallery/src/App.tsx
+++ projects/bp-gallery/src/App.tsx
@@ -3,13 +3,22 @@ import { renderRoutes, RouteConfigComponentProps } from 'react-router-config';
 import TopBar from './components/TopBar';
 import './App.scss';
 import 'react-perfect-scrollbar/dist/css/styles.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { apiBase } from './ApiConnector';
 
 const App = ({ route }: RouteConfigComponentProps) => {
+  const apolloClient = new ApolloClient({
+    uri: `${apiBase}/graphql`,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className='App'>
-      <TopBar />
-      {renderRoutes(route?.routes)}
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className='App'>
+        <TopBar />
+        {renderRoutes(route?.routes)}
+      </div>
+    </ApolloProvider>
   );
 };
 export default App;
