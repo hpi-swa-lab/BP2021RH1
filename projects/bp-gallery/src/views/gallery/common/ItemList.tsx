989b40bb8fc67ad310f81587e89a6991e682d82b
TheGrayStone <nilslissner@gmx.de>
2021-12-01 16:52:50 +0100

    Add compact mode to item list

--- projects/bp-gallery/src/views/gallery/common/ItemList.tsx
+++ projects/bp-gallery/src/views/gallery/common/ItemList.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './ItemList.scss';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Icon, IconButton } from '@mui/material';
 
 export interface ItemListItem {
   name: string;
@@ -10,27 +12,77 @@ export interface ItemListItem {
 }
 
 const ItemList = (props: { items: ItemListItem[]; compact?: boolean }) => {
+  const [scrollBarRef, setScrollBarRef] = useState<HTMLElement>();
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const listRef = useRef<HTMLDivElement>(null);
+
+  const scrollElements = (count: number) => {
+    if (!scrollBarRef) {
+      return;
+    }
+    const elementWidth = 0.22 * Math.min(window.innerHeight, window.innerWidth);
+    scrollBarRef.scroll({
+      top: 0,
+      left: scrollBarRef.scrollLeft + elementWidth * count,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className='item-list large'>
-      {props.items.map((item, index) => (
-        <div
-          className='item'
+    <div className={'item-list' + (props.compact ? ' compact' : 'large')} ref={listRef}>
+      <PerfectScrollbar
+        containerRef={ref => {
+          setScrollBarRef(ref);
+        }}
+        onScrollX={() => {
+          setScrollPosition(scrollBarRef?.scrollLeft ?? 0);
+        }}
+      >
+        {props.items.map((item, index) => (
+          <div
+            className='item'
+            onClick={() => {
+              if (item.onClick) {
+                item.onClick();
+              }
+            }}
+            key={index}
+          >
+            <div className='image-container'>
+              <img src={item.background} />
+            </div>
+            <div className='color-overlay' style={{ backgroundColor: item.color }} />
+            <div className='text-container'>
+              <span> {item.name.toUpperCase()} </span>
+            </div>
+          </div>
+        ))}
+      </PerfectScrollbar>
+      <div className='button-container'>
+        <IconButton
+          style={{ visibility: scrollPosition > 0 ? 'visible' : 'hidden' }}
           onClick={() => {
-            if (item.onClick) {
-              item.onClick();
-            }
+            scrollElements(-3);
           }}
-          key={index}
         >
-          <div className='image-container'>
-            <img src={item.background} />
-          </div>
-          <div className='color-overlay' style={{ backgroundColor: item.color }} />
-          <div className='text-container'>
-            <span> {item.name.toUpperCase()} </span>
-          </div>
-        </div>
-      ))}
+          <Icon>fast_rewind</Icon>
+        </IconButton>
+        <IconButton
+          style={{
+            visibility:
+              scrollPosition <
+              (scrollBarRef?.scrollWidth || 0) -
+                (listRef.current?.getBoundingClientRect()?.width ?? 0)
+                ? 'visible'
+                : 'hidden',
+          }}
+          onClick={() => {
+            scrollElements(3);
+          }}
+        >
+          <Icon>fast_forward</Icon>
+        </IconButton>
+      </div>
     </div>
   );
 };
