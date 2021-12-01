eb47e6979f0df703a73098ac1a41192d52e9dfa1
TheGrayStone <nilslissner@gmx.de>
2021-12-01 18:38:14 +0100

    Add api connection to search view

--- projects/bp-gallery/src/views/gallery/search/SearchView.tsx
+++ projects/bp-gallery/src/views/gallery/search/SearchView.tsx
@@ -2,31 +2,50 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import './SearchView.scss';
 import SearchHub from './SearchHub';
+import { useSearchImagesQuery } from '../../../graphql/APIConnector';
+import PictureGrid from '../common/PictureGrid';
 
 const SearchView = ({ params, scrollPos }: { params?: string[]; scrollPos: number }) => {
+  const { data, loading, error } = useSearchImagesQuery({
+    variables: {
+      text: params ? params[params.length - 1] : '',
+    },
+  });
+
+  let pics: any[] = [];
+
+  if (data?.pictures && !loading) {
+    pics = data.pictures;
+  }
+
   return (
     <div className='search-view'>
-      <div className='search-result-banner'>
-        <div className='search-result-banner-background'>
-          <img
-            style={{ transform: `translateY(${scrollPos * 0.5}px)` }}
-            src='https://bp.bad-harzburg-stiftung.de/api//uploads/1_1972_Winter04_747f834344.jpg'
-          ></img>
+      {params && params.length > 0 && (
+        <div className='search-result-banner'>
+          <div className='search-result-banner-background'>
+            <img
+              style={{ transform: `translateY(${scrollPos * 0.5}px)` }}
+              src='https://bp.bad-harzburg-stiftung.de/api//uploads/1_1972_Winter04_747f834344.jpg'
+            ></img>
+          </div>
+          <div className='search-result-breadcrumbs'>
+            {params.map((crumb: string) => {
+              return (
+                <div key={crumb} className='breadcrumb'>
+                  {crumb}
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className='search-result-breadcrumbs'>
-          {params?.map((crumb: string) => {
-            return (
-              <div key={crumb} className='breadcrumb'>
-                {crumb}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      )}
       <div className='search-content'>
         <SearchBar />
         <div className='below-search-bar'>
-          <SearchHub />
+          {loading || (!params?.length && <SearchHub />)}
+          {!loading && data && params?.length && (
+            <PictureGrid pictures={pics} hashBase={'Werner'} />
+          )}
         </div>
       </div>
     </div>
