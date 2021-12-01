471e554af0c5d419c24a8f9ff4ac4512b87015bc
TheGrayStone <nilslissner@gmx.de>
2021-12-01 16:53:43 +0100

    Add search redirect to search bar

--- projects/bp-gallery/src/views/gallery/search/SearchBar.tsx
+++ projects/bp-gallery/src/views/gallery/search/SearchBar.tsx
@@ -1,9 +1,13 @@
 import { Search } from '@mui/icons-material';
 import { InputAdornment, TextField } from '@mui/material';
 import React from 'react';
+import { History } from 'history';
+import { useHistory } from 'react-router-dom';
 import './SearchBar.scss';
 
 const SearchBar = () => {
+  const history: History = useHistory();
+
   return (
     <div className='search-bar'>
       <TextField
@@ -13,6 +17,13 @@ const SearchBar = () => {
               <Search />
             </InputAdornment>
           ),
+        }}
+        onKeyUp={event => {
+          if (event.key === 'Enter') {
+            history.push(`/search/${String((event.target as any).value)}`, {
+              showBack: true,
+            });
+          }
         }}
         placeholder='Stichworte suchen...'
         variant='outlined'
