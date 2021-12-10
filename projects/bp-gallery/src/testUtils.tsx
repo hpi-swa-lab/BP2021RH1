0de5220a0795a0b022189ea219c51d81d76259b8
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-12-10 18:49:41 +0100

    Add util-functions for test-rendering together with API mocks
    For tests we now have:
    - render (default from the testing-library)
    - renderWithAPIMocks
    - renderRoute
    - renderRouteWithAPIMocks
    
    Co-authored-by: fhradilak <franziska.hradilak@student.hpi.uni-potsdam.de>

--- projects/bp-gallery/src/testUtils.tsx
+++ projects/bp-gallery/src/testUtils.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { ReactComponentElement } from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
 import routes from './routes';
+
+/**
+ * Enables test-rendering of components with mocked GraphQL-responses
+ * @see https://www.apollographql.com/docs/react/development-testing/testing/#the-mockedprovider-component
+ */
+export const renderWithAPIMocks = (
+  component: ReactComponentElement<any>,
+  apiMocks: MockedResponse[] = []
+) => {
+  return render(
+    <MockedProvider addTypename={false} mocks={apiMocks}>
+      {component}
+    </MockedProvider>
+  );
+};
+
+const _renderRoute = (route: string, apiMocks?: MockedResponse[]) => {
+  window.history.pushState({}, 'Test page', route);
+
+  const routesContent = renderRoutes(routes);
+  const contentToWrapInRouter = apiMocks
+    ? renderWithAPIMocks(routesContent, apiMocks)
+    : routesContent;
+  return render(<BrowserRouter>{contentToWrapInRouter}</BrowserRouter>);
+};
 
 /**
  * Enables test-rendering of components based on our prior defined routes
  * @see https://testing-library.com/docs/example-react-router/
  */
 export const renderRoute = (route: string) => {
-  window.history.pushState({}, 'Test page', route);
-  return render(<BrowserRouter>{renderRoutes(routes)}</BrowserRouter>);
+  return _renderRoute(route);
+};
+
+/**
+ * Merges the functionalities of the exported util functions above
+ */
+export const renderRouteWithAPIMocks = (route: string, apiMocks: MockedResponse[] = []) => {
+  return _renderRoute(route, apiMocks);
 };
