7d3abeae01dd4577dc37856f8a2609e8cde906bd
TheGrayStone <nilslissner@gmx.de>
2021-12-15 13:49:53 +0100

    Add isActive to navigation bar

--- projects/bp-gallery/src/components/NavigationBar.tsx
+++ projects/bp-gallery/src/components/NavigationBar.tsx
@@ -8,6 +8,7 @@ export interface NavigationElement {
   name: string;
   icon: string;
   target: string | ((previousLocation: Location) => { pathname: string; hash: string; state: any });
+  isActive?: () => boolean;
 }
 
 export interface NavigationProps {
@@ -17,17 +18,24 @@ export interface NavigationProps {
 const NavigationBar = (props: NavigationProps) => {
   return (
     <div className='nav-bar'>
-      {props.elements?.map(element => (
-        <NavLink
-          to={element.target}
-          activeClassName='active'
-          key={element.name}
-          className='nav-element'
-        >
-          <Icon>{element.icon}</Icon>
-          <span className='nav-element-title'>{element.name}</span>
-        </NavLink>
-      ))}
+      {props.elements?.map(element =>
+        element.isActive ? (
+          <NavLink
+            to={element.target}
+            key={element.name}
+            isActive={element.isActive}
+            className='nav-element'
+          >
+            <Icon>{element.icon}</Icon>
+            <span className='nav-element-title'>{element.name}</span>
+          </NavLink>
+        ) : (
+          <NavLink to={element.target} key={element.name} className='nav-element'>
+            <Icon>{element.icon}</Icon>
+            <span className='nav-element-title'>{element.name}</span>
+          </NavLink>
+        )
+      )}
     </div>
   );
 };
