87adca20a8c02e784ad1db11952f68e3fb6aa189
Oliver Heß <68427341+Olliwehr@users.noreply.github.com>
2021-12-18 14:30:34 +0100

    Logo now redirects to start page (/browse) (#26)
    When not on the default browse page (/browse), the logo in the Topbar now redirects to this page when clicked.
    
    Also use the translation mechanism for the Topbar-Component.

--- projects/bp-gallery/src/components/TopBar.tsx
+++ projects/bp-gallery/src/components/TopBar.tsx
@@ -1,26 +1,38 @@
 import { Button, Icon } from '@mui/material';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { BrowserHistory } from 'history';
+import { useTranslation } from 'react-i18next';
+import { History } from 'history';
 import './TopBar.scss';
 
 const TopBar = () => {
-  const hist: BrowserHistory = useHistory();
+  const { t } = useTranslation();
+
+  const history: History = useHistory();
+  const onDefaultBrowseView = history.location.pathname.endsWith('browse');
+
   return (
     <div className='top-bar'>
       <div className='actions'>
-        {hist.location.state?.showBack && (
+        {history.location.state?.showBack && (
           <Button
             onClick={() => {
-              hist.go(-1);
+              history.go(-1);
             }}
           >
-            <Icon>arrow_back</Icon>Zurück
+            <Icon>arrow_back</Icon>
+            {t('common.back')}
           </Button>
         )}
       </div>
-      <div className='bh-logo'>
-        <img src='/bad-harzburg-stiftung-logo.png'></img>
+      <div
+        className={`bh-logo ${!onDefaultBrowseView ? 'clickable' : ''}`}
+        title={!onDefaultBrowseView ? t('common.back-to-home') : undefined}
+        onClick={
+          !onDefaultBrowseView ? () => history.push('/browse', { showBack: true }) : undefined
+        }
+      >
+        <img src='/bad-harzburg-stiftung-logo.png' alt='bh-logo' />
       </div>
     </div>
   );
