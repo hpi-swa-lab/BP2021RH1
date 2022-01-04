8fbc558ed696e541802a7a77452f9ca8cc8c1dc1
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-01-04 14:31:04 +0100

    Adds option to use cache in test-renders which is disabled by default.

--- projects/bp-gallery/src/testUtils.tsx
+++ projects/bp-gallery/src/testUtils.tsx
@@ -7,6 +7,7 @@ import routes from './routes';
 import NavigationBar, { NavigationElement } from './components/NavigationBar';
 import TopBar from './components/TopBar';
 import { NavigationContext } from './App';
+import { InMemoryCache } from '@apollo/client';
 
 /**
  * Enables using Navigation-Context in tests
@@ -30,20 +31,40 @@ const MockedApp = ({ children }: { children: any }) => {
  */
 export const renderWithAPIMocks = (
   component: ReactComponentElement<any>,
-  apiMocks: MockedResponse[] = []
+  apiMocks: MockedResponse[] = [],
+  enableCache: boolean = false
 ) => {
-  return render(_wrapInMockedProvider(component, apiMocks));
+  return render(_wrapInMockedProvider(component, apiMocks, enableCache));
 };
 
 /**
  * @private
  */
+const cache = new InMemoryCache({
+  addTypename: false,
+  typePolicies: {
+    Query: {
+      fields: {
+        pictures: {
+          // Treat picture queries as the same query, as long as the where clause is equal
+          // Queries which only differ in other fields as 'start' or 'limit' get treated as one query and the results get merged
+          keyArgs: ['where'],
+          merge(existing = [], incoming) {
+            return [...existing, ...incoming];
+          },
+        },
+      },
+    },
+  },
+});
 const _wrapInMockedProvider = (
   component: ReactComponentElement<any>,
-  apiMocks: MockedResponse[]
+  apiMocks: MockedResponse[],
+  enableCache: boolean
 ) => {
+  const optionalCache = enableCache ? cache : undefined;
   return (
-    <MockedProvider addTypename={false} mocks={apiMocks}>
+    <MockedProvider addTypename={false} mocks={apiMocks} cache={optionalCache}>
       {component}
     </MockedProvider>
   );
@@ -54,12 +75,12 @@ const _wrapInMockedProvider = (
  * @see https://testing-library.com/docs/example-react-router/
  * @private
  */
-const _renderRoute = (route: string, apiMocks?: MockedResponse[]) => {
+const _renderRoute = (route: string, apiMocks?: MockedResponse[], enableCache: boolean = false) => {
   window.history.pushState({}, 'Test page', route);
 
   const routesContent = <MockedApp>{renderRoutes(routes[0].routes)}</MockedApp>;
   const contentToWrapInRouter = apiMocks
-    ? _wrapInMockedProvider(routesContent, apiMocks)
+    ? _wrapInMockedProvider(routesContent, apiMocks, enableCache)
     : routesContent;
   return render(<BrowserRouter>{contentToWrapInRouter}</BrowserRouter>);
 };
@@ -71,6 +92,10 @@ export const renderRoute = (route: string) => {
 /**
  * Merges the functionalities of renderRoute and renderWithAPIMocks
  */
-export const renderRouteWithAPIMocks = (route: string, apiMocks: MockedResponse[] = []) => {
-  return _renderRoute(route, apiMocks);
+export const renderRouteWithAPIMocks = (
+  route: string,
+  apiMocks: MockedResponse[] = [],
+  enableCache: boolean = false
+) => {
+  return _renderRoute(route, apiMocks, enableCache);
 };
