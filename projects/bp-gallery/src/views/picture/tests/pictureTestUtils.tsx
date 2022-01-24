2fee7bc5c0662670ff4bb1108bb94038ad50eaf9
TheGrayStone <nilslissner@gmx.de>
2022-01-24 13:01:13 +0100

    Add utils

--- projects/bp-gallery/src/views/picture/tests/pictureTestUtils.tsx
+++ projects/bp-gallery/src/views/picture/tests/pictureTestUtils.tsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import { render } from '@testing-library/react';
 import { ReactComponentElement } from 'react';
-import { PictureViewContext } from '../PictureView';
+import { PictureViewContext, PictureViewContextFields } from '../PictureView';
 
-const MockedPictureView = ({ children }: { children: any }) => {
+const MockedPictureView = ({
+  state,
+  children,
+}: {
+  state?: PictureViewContextFields;
+  children: any;
+}) => {
   const [sideBarOpen, setSideBarOpen] = useState<boolean>(false);
 
   return (
     <PictureViewContext.Provider
-      value={{
-        sideBarOpen,
-        setSideBarOpen,
-      }}
+      value={
+        state ?? {
+          sideBarOpen,
+          setSideBarOpen,
+        }
+      }
     >
       {children}
     </PictureViewContext.Provider>
   );
 };
 
-export const renderWithPictureContextMocks = (component: ReactComponentElement<any>) => {
-  return render(<MockedPictureView>{component}</MockedPictureView>);
+export const renderWithPictureContextMocks = (
+  component: ReactComponentElement<any>,
+  state?: PictureViewContextFields
+) => {
+  return render(<MockedPictureView state={state}>{component}</MockedPictureView>);
 };
