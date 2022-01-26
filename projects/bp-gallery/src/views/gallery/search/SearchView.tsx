0dd4ad420b9b048609b7208007018443fc8305ce
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-01-26 13:46:07 +0100

    Extract conversion of SearchParams to PictureFilterInputs
    Also adds tests for this conversion process :)
    Note that 2 tests arent passing, as I detected a slightly difference in the time-range-format we should send to the backend.

--- projects/bp-gallery/src/views/gallery/search/SearchView.tsx
+++ projects/bp-gallery/src/views/gallery/search/SearchView.tsx
@@ -25,6 +25,63 @@ export const asSearchPath = (
   return `/search?${searchParams.toString()}`;
 };
 
+export const convertSearchParamsToPictureFilters = (searchParams: URLSearchParams) => {
+  const filters: PictureFiltersInput = { and: [] };
+
+  if (searchParams.has(SearchType.DECADE)) {
+    if (searchParams.get(SearchType.DECADE) === 'pre50') searchParams.set(SearchType.DECADE, '40');
+    const decade = parseInt(searchParams.get(SearchType.DECADE) ?? '');
+
+    if (!isNaN(decade)) {
+      let startTime;
+      if (decade === 40) {
+        startTime = new Date(`1900-01-01`);
+      } else {
+        startTime = new Date(`19${decade / 10}0-01-01`);
+      }
+      const endTime = new Date(`19${decade / 10}9-12-31`);
+      filters.and?.push({
+        time_range_tag: {
+          start: {
+            gte: dayjs(startTime).format('YYYY-MM-DDTHH:mm:ssZ'),
+          },
+          end: {
+            lte: dayjs(endTime).format('YYYY-MM-DDTHH:mm:ssZ'),
+          },
+        },
+      });
+    }
+  }
+
+  if (searchParams.has(SearchType.KEYWORD)) {
+    const keywords = searchParams.getAll(SearchType.KEYWORD).map(decodeURIComponent);
+    keywords.forEach((keyword: string) => {
+      filters.and?.push({
+        keyword_tags: {
+          name: {
+            containsi: keyword,
+          },
+        },
+      });
+    });
+  }
+
+  if (searchParams.has(SearchType.DEFAULT)) {
+    const q = searchParams.getAll(SearchType.DEFAULT).map(decodeURIComponent);
+    q.forEach((param: string) => {
+      filters.and?.push({
+        descriptions: {
+          text: {
+            containsi: param,
+          },
+        },
+      });
+    });
+  }
+
+  return filters;
+};
+
 const SearchView = ({ scrollPos, scrollHeight }: { scrollPos: number; scrollHeight: number }) => {
   const [searchSnippet, setSearchSnippet] = useState<string>('');
   const [previewPicture, setPreviewPicture] = useState<FlatPicture>();
@@ -35,63 +92,10 @@ const SearchView = ({ scrollPos, scrollHeight }: { scrollPos: number; scrollHeig
   }, [search]);
 
   // Builds query from search params in the path
-  const filtersClause = useMemo(() => {
-    const filters: { and: PictureFiltersInput[] } = { and: [] };
-
-    if (searchParams.has(SearchType.DECADE)) {
-      if (searchParams.get(SearchType.DECADE) === 'pre50')
-        searchParams.set(SearchType.DECADE, '40');
-      const decade = parseInt(searchParams.get(SearchType.DECADE) ?? '');
-
-      if (!isNaN(decade)) {
-        let startTime;
-        if (decade === 40) {
-          startTime = new Date(`1900-01-01`);
-        } else {
-          startTime = new Date(`19${decade / 10}0-01-01`);
-        }
-        const endTime = new Date(`19${decade / 10}9-12-31`);
-        filters['and'].push({
-          time_range_tag: {
-            start: {
-              gte: dayjs(startTime).format('YYYY-MM-DDTHH:mm:ssZ'),
-            },
-            end: {
-              lte: dayjs(endTime).format('YYYY-MM-DDTHH:mm:ssZ'),
-            },
-          },
-        });
-      }
-    }
-
-    if (searchParams.has(SearchType.KEYWORD)) {
-      const keywords = searchParams.getAll(SearchType.KEYWORD).map(decodeURIComponent);
-      keywords.forEach((keyword: string) => {
-        filters['and'].push({
-          keyword_tags: {
-            name: {
-              containsi: keyword,
-            },
-          },
-        });
-      });
-    }
-
-    if (searchParams.has(SearchType.DEFAULT)) {
-      const q = searchParams.getAll(SearchType.DEFAULT).map(decodeURIComponent);
-      q.forEach((param: string) => {
-        filters['and'].push({
-          descriptions: {
-            text: {
-              containsi: param,
-            },
-          },
-        });
-      });
-    }
-
-    return filters;
-  }, [searchParams]);
+  const filtersClause = useMemo(
+    () => convertSearchParamsToPictureFilters(searchParams),
+    [searchParams]
+  );
 
   return (
     <div className='search-view'>
