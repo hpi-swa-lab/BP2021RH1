55268dfd6321d664b046e0623289a05f6c21cc70
BenediktV <37239082+BenediktV@users.noreply.github.com>
2022-01-26 14:52:20 +0100

    Uses 'and' keyword to implement nested search (#43)
    * Uses 'and' keyword to implement nested search
    
    * remove TODO comments
    
    * adjust mocks to slightly new query format
    
    * Implements review feedback
    
    * Extract conversion of SearchParams to PictureFilterInputs
    
    Also adds tests for this conversion process :)
    Note that 2 tests arent passing, as I detected a slightly difference in the time-range-format we should send to the backend.
    
    * Use of 'YYYY-MM-DDTHH:mm:ssZ' DateTime-format for picture-filtering
    
    Now is send to the backend in correct UTC+0 format
    
    Co-authored-by: Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>

--- projects/bp-gallery/src/views/gallery/search/tests/SearchView.test.tsx
+++ projects/bp-gallery/src/views/gallery/search/tests/SearchView.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import { renderRoute, renderRouteWithAPIMocks } from '../../../../testUtils';
 import { GetPicturesSearchMocks } from './mocks';
+import { convertSearchParamsToPictureFilters } from '../SearchView';
 
 const SearchHubMock = () => <div>SearchHubMock</div>;
 jest.mock('../searchHub/SearchHub', () => SearchHubMock);
@@ -85,6 +86,169 @@ describe('SearchView called with parameters which match at least one picture', (
   it('should not render the SearchHub', async () => {
     await waitFor(() => {
       expect(() => screen.getAllByText('SearchHubMock')).toThrow();
+    });
+  });
+});
+
+describe('convertSearchParamsToPictureFilters', () => {
+  it('should append time range tag filter', () => {
+    const searchParams = new URLSearchParams({ decade: '70' });
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          time_range_tag: {
+            start: {
+              gte: '1970-01-01T00:00:00Z',
+            },
+            end: {
+              lte: '1979-12-31T23:59:59Z',
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append time range tag filter for pre50 decade', () => {
+    const searchParams = new URLSearchParams({ decade: 'pre50' });
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          time_range_tag: {
+            start: {
+              gte: '1900-01-01T00:00:00Z',
+            },
+            end: {
+              lte: '1949-12-31T23:59:59Z',
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append single keyword tag filter with simple name', () => {
+    const keyword = 'Burgberg';
+    const searchParams = new URLSearchParams({ keyword });
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          keyword_tags: {
+            name: {
+              containsi: keyword,
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append single keyword tag filter with URL encoded name', () => {
+    const keyword = 'Bad Harzburg';
+    const searchParams = new URLSearchParams({
+      keyword: encodeURIComponent(keyword),
+    });
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          keyword_tags: {
+            name: {
+              containsi: keyword,
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append multiple keyword tag filters', () => {
+    const keyword1 = 'Burgberg';
+    const keyword2 = 'Winterberg';
+    const searchParams = new URLSearchParams({ keyword: keyword1 });
+    searchParams.append('keyword', keyword2);
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          keyword_tags: {
+            name: {
+              containsi: keyword1,
+            },
+          },
+        },
+        {
+          keyword_tags: {
+            name: {
+              containsi: keyword2,
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append single description filter', () => {
+    const description = 'Onkel Pelle';
+    const searchParams = new URLSearchParams({
+      q: encodeURIComponent(description),
+    });
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          descriptions: {
+            text: {
+              containsi: description,
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('should append multiple description filters', () => {
+    const description1 = 'Onkel Pelle';
+    const description2 = 'Winterberg';
+    const searchParams = new URLSearchParams({
+      q: encodeURIComponent(description1),
+    });
+    searchParams.append('q', encodeURIComponent(description2));
+
+    const result = convertSearchParamsToPictureFilters(searchParams);
+
+    expect(result).toEqual({
+      and: [
+        {
+          descriptions: {
+            text: {
+              containsi: description1,
+            },
+          },
+        },
+        {
+          descriptions: {
+            text: {
+              containsi: description2,
+            },
+          },
+        },
+      ],
     });
   });
 });
