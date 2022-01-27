8d1ac49908e20f0c6a2bc87ec11bb5c4b311331d
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-01-27 16:47:41 +0100

    Highly questionable addition of alert messages on login and logout

--- projects/bp-gallery/src/components/NavigationBar.tsx
+++ projects/bp-gallery/src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, Icon } from '@mui/material';
+import { Alert, Button, Dialog, Icon, Snackbar } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavigationBar.scss';
@@ -8,32 +8,43 @@ import { authRole, useAuth } from '../AuthWrapper';
 
 const NavigationBar = () => {
   const [openLogin, setOpenLogin] = useState<boolean>(false);
+  const [successAlertMessage, setSuccessAlertMessage] = useState<string | undefined>(undefined);
   const { t } = useTranslation();
 
   const { role, logout } = useAuth();
 
+  // When a user successfully logs in, the Dialog closes and a success-message gets displayed
   useEffect(() => {
+    if (role === authRole.PUBLIC) return;
     setOpenLogin(false);
-  }, [role]);
+    setSuccessAlertMessage(t('common.successful-login'));
+  }, [role, t]);
+
+  const onLogout = () => {
+    logout();
+    setSuccessAlertMessage(t('common.successful-logout'));
+  };
 
   return (
-    <div className='nav-bar'>
-      <NavLink to={'/browse'} className='nav-element'>
-        <Icon>book</Icon>
-        <span className='nav-element-title'>{t('common.browse')}</span>
-      </NavLink>
-      <NavLink to={'/search'} className='nav-element'>
-        <Icon>search</Icon>
-        <span className='nav-element-title'>{t('common.search')}</span>
-      </NavLink>
-      <div
-        className='nav-element'
-        onClick={role === authRole.PUBLIC ? () => setOpenLogin(true) : logout}
-      >
-        <Icon>login</Icon>
-        <span className='nav-element-title'>
-          {role === authRole.PUBLIC ? t('common.login') : t('common.logout')}
-        </span>
+    <>
+      <div className='nav-bar'>
+        <NavLink to={'/browse'} className='nav-element'>
+          <Icon>book</Icon>
+          <span className='nav-element-title'>{t('common.browse')}</span>
+        </NavLink>
+        <NavLink to={'/search'} className='nav-element'>
+          <Icon>search</Icon>
+          <span className='nav-element-title'>{t('common.search')}</span>
+        </NavLink>
+        <div
+          className='nav-element'
+          onClick={role === authRole.PUBLIC ? () => setOpenLogin(true) : onLogout}
+        >
+          <Icon>login</Icon>
+          <span className='nav-element-title'>
+            {role === authRole.PUBLIC ? t('common.login') : t('common.logout')}
+          </span>
+        </div>
       </div>
       <Dialog open={openLogin} fullWidth={true} onBackdropClick={() => setOpenLogin(false)}>
         <Button onClick={() => setOpenLogin(false)} className='close-button'>
@@ -41,7 +52,15 @@ const NavigationBar = () => {
         </Button>
         <LoginScreen />
       </Dialog>
-    </div>
+      <Snackbar
+        open={successAlertMessage !== undefined}
+        autoHideDuration={2000}
+        onClose={() => setSuccessAlertMessage(undefined)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert severity='success'>{successAlertMessage}</Alert>
+      </Snackbar>
+    </>
   );
 };
 
