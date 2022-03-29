0214e97fdfdec37f20f60c3d51f4f642abdd5c6a
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-03-29 14:41:48 +0000

    Clean up commit
    * Delete category tags & title
    * Add browse root collection
    * Add collection
    * Various changes for required attributes in other schemas

---
+++ projects/bp-strapi/src/api/browse-root-collection/controllers/browse-root-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  browse-root-collection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::browse-root-collection.browse-root-collection');
--- projects/bp-strapi/src/api/category-tag/services/category-tag.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * category-tag service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::category-tag.category-tag');
