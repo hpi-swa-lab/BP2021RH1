4ca07fefdec4c750dcc395004dc2ecf7b6f1c5ca
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-03-31 15:08:00 +0200

    remove unused import

--- projects/bp-gallery/src/views/gallery/common/PictureScrollGrid.tsx
+++ projects/bp-gallery/src/views/gallery/common/PictureScrollGrid.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import { PictureFiltersInput, useGetPicturesQuery } from '../../../graphql/APIConnector';
 import { useSimplifiedQueryResponseData } from '../../../graphql/queryUtils';
 import { FlatPicture } from '../../../graphql/additionalFlatTypes';
