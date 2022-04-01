58df0c23da1de4d6ffa1d6ee4d7448a4044ab605
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-04-01 11:45:07 +0000

    Add thumbnail custom resolver
    * Add an (empty) field "thumbnail" to collections
    * Use a custom GraphQL resolver to dynamically fill this field on request

--- projects/bp-strapi/src/index.js
+++ projects/bp-strapi/src/index.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const resolveThumbnail = async (strapi, collectionId, alreadySeenIds) => {
+  alreadySeenIds.push(collectionId);
+  const collectionQuery = strapi.db.query('api::collection.collection');
+  const response = await collectionQuery.findOne({
+    where: {
+      id: collectionId
+    },
+    select: ['id'],
+    populate: {
+      pictures: {
+        select: ['id'],
+        limit: 1,
+        populate: {
+          media: {
+            select: ['formats']
+          }
+        },
+      },
+      child_collections: {
+        select: ['id']
+      }
+    },
+  });
+  if (response.pictures[0] && response.pictures[0].media.formats.thumbnail.url) {
+    return response.pictures[0].media.formats.thumbnail.url;
+  } else {
+    for (const child of response.child_collections) {
+      if (alreadySeenIds.includes(child.id)) {continue; }
+      const thumb = await resolveThumbnail(strapi, child.id, alreadySeenIds);
+      if (thumb) {
+        return thumb;
+      }
+    }
+  }
+  return null;
+}
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -7,7 +44,31 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin('graphql').service('extension');
+
+    const extension = (gqlExtensions) => ({
+      resolversConfig: {
+        'Collection.thumbnail': {
+          middlewares: [
+            async (_, parent) => {
+              return await resolveThumbnail(gqlExtensions.strapi, parent.id, []);
+            },
+          ],
+          auth: {
+            scope: [
+              'api::collection.collection.find',
+              'api::collection.collection.findOne',
+              'api::picture.picture.find',
+              'api::picture.picture.findOne'
+            ]
+          }
+        }
+      },
+    });
+
+    extensionService.use(extension);
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
