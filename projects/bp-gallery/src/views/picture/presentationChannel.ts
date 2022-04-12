7d2e09fa893151bbda3d57e772c4723f4e46adde
TheGrayStone <nilslissner@gmx.de>
2022-04-12 10:31:18 +0200

    Safari fallback for broadcast channel

--- projects/bp-gallery/src/views/picture/presentationChannel.ts
+++ projects/bp-gallery/src/views/picture/presentationChannel.ts
@@ -1,8 +1,59 @@
+import EventEmitter from 'events';
 import { useCallback, useEffect, useMemo } from 'react';
 
+/* ========== FALLBACK FOR SAFARI ========== */
+const fallbackLocalSingleton = new EventEmitter();
+
+class FallbackChannel {
+  onmessage?: (event: any) => void;
+  id: string;
+  closed = false;
+
+  constructor(id: string) {
+    this.id = id;
+    window.addEventListener('storage', event => {
+      if (event.key !== `__fallbackChannel_${this.id}` || !this.onmessage || !event.newValue)
+        return;
+      this.onmessage({
+        data: JSON.parse(event.newValue),
+      });
+    });
+    fallbackLocalSingleton.on('post', event => {
+      if (this.onmessage) {
+        this.onmessage(event);
+      }
+    });
+  }
+
+  postMessage(msg: any) {
+    if (!this.closed) {
+      localStorage.setItem(`__fallbackChannel_${this.id}`, JSON.stringify(msg));
+      fallbackLocalSingleton.emit('post', {
+        data: msg,
+      });
+    }
+  }
+
+  close() {
+    localStorage.removeItem(`__fallbackChannel_${this.id}`);
+    this.closed = true;
+  }
+}
+/* ========== FALLBACK END ========== */
+
 const usePresentationChannel = (id: string, onNavigate: (pictureId: string) => void) => {
-  const producer = useMemo(() => new BroadcastChannel(`presentation-${id}`), [id]);
-  const consumer = useMemo(() => new BroadcastChannel(`presentation-${id}`), [id]);
+  const channelFactory = (id: string) => {
+    if (BroadcastChannel as any) {
+      return new BroadcastChannel(id);
+    } else {
+      console.debug(
+        'Broadcast Channel not found on this system. Falling back to using local storage.'
+      );
+      return new FallbackChannel(id);
+    }
+  };
+  const producer = useMemo(() => channelFactory(id), [id]);
+  const consumer = useMemo(() => channelFactory(id), [id]);
 
   useEffect(() => {
     consumer.onmessage = (event: MessageEvent<{ pictureId: string }>) => {
