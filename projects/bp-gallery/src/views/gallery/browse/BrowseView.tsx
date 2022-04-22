ce6afb4e2c97232379e2c53d0340e825dc913363
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-04-22 14:06:06 +0200

    Remove unused imports

--- projects/bp-gallery/src/views/gallery/browse/BrowseView.tsx
+++ projects/bp-gallery/src/views/gallery/browse/BrowseView.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import './CollectionPictureDisplay.scss';
-import { History } from 'history';
-import { useHistory } from 'react-router-dom';
 import {
   useGetCollectionInfoQuery,
   useGetCollectionWithPicturesPublishedAfterQuery,
