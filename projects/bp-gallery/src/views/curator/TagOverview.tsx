f4c51876c39bec260865285dab43918800953277
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-05-05 16:32:48 +0200

    Add option to merge 2 tags and to delete tags

--- projects/bp-gallery/src/views/curator/TagOverview.tsx
+++ projects/bp-gallery/src/views/curator/TagOverview.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useMemo } from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 import './TagOverview.scss';
 import { ComponentCommonSynonyms, ComponentCommonSynonymsInput } from '../../graphql/APIConnector';
 import QueryErrorDisplay from '../shared/QueryErrorDisplay';
@@ -12,15 +12,18 @@ import {
   GridRowsProp,
 } from '@mui/x-data-grid';
 import { AlertContext, AlertType } from '../shared/AlertWrapper';
-import { Chip } from '@mui/material';
+import { Button, Chip, IconButton } from '@mui/material';
 import useGenericTagEndpoints from './endpoints';
 import { useTranslation } from 'react-i18next';
+import { Delete } from '@mui/icons-material';
+import { DialogContext, DialogPreset } from '../shared/DialogWrapper';
 
 interface TagRow {
   id: string;
   name: string;
   synonyms: { synonyms: ComponentCommonSynonyms[]; tagId: string };
   add: string;
+  delete: { tagId: string };
 }
 
 interface FlatTag {
@@ -31,11 +34,19 @@ interface FlatTag {
 
 const TagOverview = ({ type }: { type: string }) => {
   const openAlert = useContext(AlertContext);
+  const prompt = useContext(DialogContext);
   const { t } = useTranslation();
+  const [selectedRowIds, setSelectedRowIds] = useState<string[]>([]);
 
-  const { tagQuery, updateTagNameMutationSource, updateSynonymsMutationSource } =
-    useGenericTagEndpoints(type);
-  const { data, loading, error, refetch } = tagQuery();
+  const {
+    allTagsQuery,
+    updateTagNameMutationSource,
+    updateSynonymsMutationSource,
+    mergeTagsMutationSource,
+    deleteTagMutationSource,
+  } = useGenericTagEndpoints(type);
+
+  const { data, loading, error, refetch } = allTagsQuery();
   const flattened = useSimplifiedQueryResponseData(data);
   const flattenedTags: FlatTag[] | undefined = flattened ? Object.values(flattened)[0] : undefined;
 
@@ -56,6 +67,20 @@ const TagOverview = ({ type }: { type: string }) => {
   });
 
   const [updateTagNameMutation] = updateTagNameMutationSource({
+    errorPolicy: 'all',
+    onCompleted: _ => {
+      refetch();
+    },
+  });
+
+  const [mergeTagsMutation] = mergeTagsMutationSource({
+    errorPolicy: 'all',
+    onCompleted: _ => {
+      refetch();
+    },
+  });
+
+  const [deleteTagMutation] = deleteTagMutationSource({
     errorPolicy: 'all',
     onCompleted: _ => {
       refetch();
@@ -91,6 +116,25 @@ const TagOverview = ({ type }: { type: string }) => {
     [updateSynonymsMutation, tags]
   );
 
+  const deleteTag = useCallback(
+    async (tagId?: string, tagName?: string) => {
+      if (!tagId || !tagName) return;
+      const reallyDelete = await prompt({
+        preset: DialogPreset.CONFIRM,
+        title: t('curator.reallyDeleteTag'),
+        content: tagName,
+      });
+      if (reallyDelete) {
+        deleteTagMutation({
+          variables: {
+            id: tagId,
+          },
+        });
+      }
+    },
+    [deleteTagMutation, prompt, t]
+  );
+
   const processRowUpdate = useCallback(
     (row: GridRowModel<TagRow>) => {
       if (row.add !== '') {
@@ -110,11 +154,11 @@ const TagOverview = ({ type }: { type: string }) => {
 
   const columns: GridColDef[] = useMemo(
     () => [
-      { field: 'name', headerName: 'Name', flex: 1, editable: true },
+      { field: 'name', headerName: 'Name', flex: 2, editable: true },
       {
         field: 'synonyms',
         headerName: t('curator.synonyms'),
-        flex: 3,
+        flex: 6,
         renderCell: (
           params: GridRenderCellParams<{
             synonyms: ComponentCommonSynonyms[];
@@ -133,11 +177,32 @@ const TagOverview = ({ type }: { type: string }) => {
       {
         field: 'add',
         headerName: t('curator.add'),
-        flex: 1,
+        flex: 2,
         editable: true,
       },
+      {
+        field: 'delete',
+        headerName: t('common.delete'),
+        flex: 1,
+        renderCell: (
+          params: GridRenderCellParams<{
+            tagId: string;
+            tagName: string;
+          }>
+        ) => {
+          return (
+            <IconButton
+              onClick={() => {
+                deleteTag(params.value?.tagId, params.value?.tagName);
+              }}
+            >
+              <Delete />
+            </IconButton>
+          );
+        },
+      },
     ],
-    [t, deleteSynonym]
+    [deleteTag, t, deleteSynonym]
   );
 
   const rows: GridRowsProp = useMemo(
@@ -148,10 +213,36 @@ const TagOverview = ({ type }: { type: string }) => {
           name: tag.name,
           synonyms: { synonyms: tag.synonyms, tagId: tag.id },
           add: '',
+          delete: { tagId: tag.id, tagName: tag.name },
         } as TagRow;
       }),
     [tags]
   );
+
+  const getSelectedRows = useCallback(
+    (selectedIds: string[]) => {
+      const ids = new Set(selectedIds);
+      return rows.filter(row => ids.has(row.id));
+    },
+    [rows]
+  );
+
+  const mergeTags = useCallback(() => {
+    if (selectedRowIds.length !== 2) {
+      openAlert({
+        alertType: AlertType.ERROR,
+        message: t('curator.mergeLimit'),
+      });
+    } else {
+      const selectedRows: GridRowsProp = getSelectedRows(selectedRowIds);
+      mergeTagsMutation({
+        variables: {
+          targetId: selectedRows[0].id as string,
+          sourceId: selectedRows[1].id as string,
+        },
+      });
+    }
+  }, [openAlert, mergeTagsMutation, selectedRowIds, getSelectedRows, t]);
 
   if (error) {
     return <QueryErrorDisplay error={error} />;
@@ -160,6 +251,7 @@ const TagOverview = ({ type }: { type: string }) => {
   } else if (Object.values(tags).length) {
     return (
       <div className='grid'>
+        <Button onClick={mergeTags}>merge</Button>
         <DataGrid
           rows={rows}
           columns={columns}
@@ -171,6 +263,12 @@ const TagOverview = ({ type }: { type: string }) => {
               message: error.message,
             });
           }}
+          checkboxSelection={true}
+          selectionModel={selectedRowIds}
+          onSelectionModelChange={newSelectionModel => {
+            setSelectedRowIds(newSelectionModel as string[]);
+          }}
+          disableSelectionOnClick={true}
         />
       </div>
     );
