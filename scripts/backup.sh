67c3d20eb419586e88bd407aaf4a955a3c2e1b60
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-05-25 11:39:42 +0000

    Improve backup script and use incremental backups to save on storage space

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+############ VARIABLE DEFINITIONS ############
+BACKUP_DIR=/home/dev/backups/
+MEDIA_DIR=/home/dev/BP2021RH1/projects/bp-strapi/public/uploads
+DB_NAME=bp-strapi
+DB_USER=postgres
+##############################################
+
+
+############ CODE ############
+# Find the last backup, if none exists, it will just continue to make a full backup
+LAST_BACKUP_FOLDER=$(ls -td -- "$BACKUP_DIR"* | head -n 1)
+
+echo "last backup is placed in folder $LAST_BACKUP_FOLDER"
+
+# Create new directory for backup placement
 TIMESTAMP=$(date +%Y-%m-%d-%H%M%S-%3N)
+BACKUP_TARGET="$BACKUP_DIR$TIMESTAMP"
+mkdir $BACKUP_TARGET
 
-mkdir  /home/dev/backups/$TIMESTAMP
-pg_dump -U postgres -Fc bp-strapi > /home/dev/backups/$TIMESTAMP/db_backup.sql
+# Dump contents of the database
+pg_dump -U $DB_USER -Fc $DB_NAME > "$BACKUP_TARGET/db_backup.sql"
 
-tar -cf /home/dev/backups/$TIMESTAMP/uploads.tar.gz /home/dev/BP2021RH1/projects/bp-strapi/public/uploads
+# Sync picture files using incremental backup system to backup folder
+rsync -a -v --delete --link-dest=$LAST_BACKUP_FOLDER $MEDIA_DIR $BACKUP_TARGET
 
-find /home/dev/backups/* -mtime +14 -type d -exec rm -r {} +
+# Delte stale (> 14 days old) backups
+find "$BACKUP_DIR"* -mtime +14 -type d -exec rm -r {} +
+##############################################
