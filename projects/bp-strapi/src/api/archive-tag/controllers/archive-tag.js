b9c1e152509eb2fd26e7c5d9db977546a46a8e01
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-05-30 13:47:03 +0000

    Add new archive-tag collection type

---
+++ projects/bp-strapi/src/api/archive-tag/controllers/archive-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  archive-tag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::archive-tag.archive-tag');
