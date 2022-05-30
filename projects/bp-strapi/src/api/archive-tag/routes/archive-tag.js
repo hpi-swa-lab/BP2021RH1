24376cb05a91785024cf956bb4f7e3616cc8d3c2
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2022-05-30 13:47:03 +0000

    Add new archive-tag collection type

---
+++ projects/bp-strapi/src/api/archive-tag/routes/archive-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * archive-tag router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::archive-tag.archive-tag');
---
+++ projects/bp-strapi/src/api/archive-tag/services/archive-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * archive-tag service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::archive-tag.archive-tag');
