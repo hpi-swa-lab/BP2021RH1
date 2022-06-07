9a5d3bb694801ccb6d91b60b1d935c49736fc647
Benedikt Vidic <benedikt.vidic@gmx.de>
2022-06-07 16:40:29 +0200

    Add dialog to create new archive

--- projects/bp-gallery/src/components/views/uploads/UploadsView.tsx
+++ projects/bp-gallery/src/components/views/uploads/UploadsView.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
 import PictureScrollGrid from '../../common/picture-gallery/PictureScrollGrid';
 import { FlatPicture } from '../../../types/additionalFlatTypes';
 import { AuthRole, useAuth } from '../../provider/AuthProvider';
@@ -6,10 +6,16 @@ import './UploadsView.scss';
 import { useTranslation } from 'react-i18next';
 import useBulkOperations from '../../../hooks/bulk-operations.hook';
 import ScrollContainer from '../../common/ScrollContainer';
+import { Button, Icon } from '@mui/material';
+import { DialogContext, DialogPreset } from '../../provider/DialogProvider';
+import { useCreateArchiveTagMutation } from '../../../graphql/APIConnector';
 
 const UploadsView = () => {
   const { role } = useAuth();
   const { t } = useTranslation();
+  const dialog = useContext(DialogContext);
+
+  const [createArchiveTagMutation] = useCreateArchiveTagMutation();
 
   const { moveToCollection } = useBulkOperations();
 
@@ -22,10 +28,31 @@ const UploadsView = () => {
         }
       : undefined;
   }, [role, t]);
+
+  const createArchive = useCallback(async () => {
+    console.log('Test');
+    const archiveName = await dialog({
+      title: t('curator.createArchive'),
+      preset: DialogPreset.INPUT_FIELD,
+    });
+    if (archiveName?.length) {
+      createArchiveTagMutation({
+        variables: {
+          name: archiveName,
+        },
+      });
+    }
+  }, [createArchiveTagMutation, dialog, t]);
+
+  if (role < AuthRole.CURATOR) return null;
   return (
     <ScrollContainer>
       {(scrollPos: number, scrollHeight: number) => (
         <div className='uploads-overview'>
+          <Button onClick={createArchive}>
+            <Icon>add</Icon>
+            {t('curator.createArchive')}
+          </Button>
           <PictureScrollGrid
             queryParams={{ collections: { id: { null: true } } }}
             scrollPos={scrollPos}
