ef3ee4081ceccb30ceea18a7814f06a050bf17a9
TheGrayStone <nilslissner@gmx.de>
2022-06-15 10:57:15 +0200

    Prepare for main production deployment

--- projects/bp-strapi/ecosystem.config.js
+++ projects/bp-strapi/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
     production: {
       key: 'deploy.key',
       user: 'github',
-      ref: 'origin/strapi-deployment',
+      ref: 'origin/main',
       host: 'bp.bad-harzburg-stiftung.de',
       path: '/home/github/BP2021RH1',
       repo: 'https://github.com/hpi-swa-lab/BP2021RH1',
