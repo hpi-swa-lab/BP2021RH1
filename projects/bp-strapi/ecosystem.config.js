1d1f02f69a1f69de310e59491aa02bee445f9d67
TheGrayStone <nilslissner@gmx.de>
2022-06-15 11:09:45 +0200

    Add config for staging

--- projects/bp-strapi/ecosystem.config.js
+++ projects/bp-strapi/ecosystem.config.js
@@ -17,6 +17,15 @@ module.exports = {
       path: '/home/github/BP2021RH1',
       repo: 'https://github.com/hpi-swa-lab/BP2021RH1',
       'post-deploy': 'cd projects/bp-strapi && yarn install && yarn build:prod && yarn pm2 reload ecosystem.config.js --env production && yarn pm2 save && git checkout yarn.lock'
+    },
+    staging: {
+      key: 'deploy.key',
+      user: 'github',
+      ref: 'origin/staging',
+      host: 'bp.bad-harzburg-stiftung.de',
+      path: '/home/github/BP2021RH1-staging',
+      repo: 'https://github.com/hpi-swa-lab/BP2021RH1',
+      'post-deploy': 'cd projects/bp-strapi && yarn install && yarn build && yarn pm2 reload ecosystem.config.js --env staging && yarn pm2 save && git checkout yarn.lock'
     }
   }
 }
