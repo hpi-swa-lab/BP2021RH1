87d227f970e68429eeaee719823551ea36f7092b
TheGrayStone <nilslissner@gmx.de>
2022-06-15 13:38:51 +0200

    Add env vars to workflow files

--- projects/bp-strapi/ecosystem.config.js
+++ projects/bp-strapi/ecosystem.config.js
@@ -11,18 +11,18 @@ module.exports = {
   deploy: {
     production: {
       key: 'deploy.key',
-      user: 'github',
+      user: process.env.REMOTE_USER,
       ref: 'origin/main',
-      host: 'bp.bad-harzburg-stiftung.de',
+      host: process.env.REMOTE_HOST,
       path: '/home/github/BP2021RH1',
       repo: 'https://github.com/hpi-swa-lab/BP2021RH1',
       'post-deploy': 'cd projects/bp-strapi && yarn install && yarn build:prod && yarn pm2 reload ecosystem.config.js --env production && yarn pm2 save && git checkout yarn.lock'
     },
     staging: {
       key: 'deploy.key',
-      user: 'github',
+      user: process.env.REMOTE_USER,
       ref: 'origin/staging',
-      host: 'bp.bad-harzburg-stiftung.de',
+      host: process.env.REMOTE_HOST,
       path: '/home/github/BP2021RH1-staging',
       repo: 'https://github.com/hpi-swa-lab/BP2021RH1',
       'post-deploy': 'cd projects/bp-strapi && yarn install && yarn build && yarn pm2 reload ecosystem.config.js --env staging && yarn pm2 save && git checkout yarn.lock'
