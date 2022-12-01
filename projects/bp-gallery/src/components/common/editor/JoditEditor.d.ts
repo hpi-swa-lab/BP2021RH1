83300a8b8f71f7816a059c919e8e5c865aa5d7c0
olschulz <64468276+olschulz@users.noreply.github.com>
2022-12-01 14:39:13 +0100

    fix lint errors

--- projects/bp-gallery/src/components/common/editor/JoditEditor.d.ts
+++ projects/bp-gallery/src/components/common/editor/JoditEditor.d.ts
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { IJodit } from 'jodit';
 
-// declare module 'JoditEditor' {
 export interface IJoditEditorProps {
   value: string;
   config?: IJodit['options'];
   onChange: (newValue: string) => void;
   onBlur: (newValue: string) => void;
 }
+
 declare const JoditEditor: React.ComponentType<IJoditEditorProps>;
 export default JoditEditor;
-// }
