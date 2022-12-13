08eb06e087d918b48c7af0eebe866c850ad76e7d
olschulz <64468276+olschulz@users.noreply.github.com>
2022-12-13 17:02:09 +0100

    show and edit description, show archive pics

---
+++ projects/bp-gallery/src/components/views/archives/ArchiveInfo.tsx
@@ -0,0 +1,119 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { sanitize } from 'dompurify';
+import './ArchiveInfo.scss';
+import { Icon, IconButton } from '@mui/material';
+import getLineBreaks from '../../../helpers/get-linebreaks';
+import { AuthRole, useAuth } from '../../provider/AuthProvider';
+import { useUpdateArchiveMutation } from '../../../graphql/APIConnector';
+import { useTranslation } from 'react-i18next';
+import { FlatArchiveTag } from '../../../types/additionalFlatTypes';
+import Editor from '../../common/editor/Editor';
+
+interface ArchiveInfoProps {
+  archive: FlatArchiveTag;
+}
+
+const ArchiveInfo = ({ archive }: ArchiveInfoProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { role } = useAuth();
+  const description = archive.description as string;
+  const id = archive.id;
+  const name = archive.name;
+
+  const isDescriptionLong = useMemo(() => {
+    const buffer = document.createElement('div');
+    buffer.className = 'collection-description open';
+    buffer.innerText = description;
+    document.body.appendChild(buffer);
+    const split = getLineBreaks(buffer.childNodes[0]);
+    buffer.remove();
+
+    return split.length > 3;
+  }, [description]);
+
+  return (
+    <div className='collection-container'>
+      <h2>{name}</h2>
+      {role >= AuthRole.CURATOR ? (
+        <EditableArchiveInfo initialDescription={description} archiveId={id} />
+      ) : (
+        <>
+          {description && (
+            <div
+              className={
+                isOpen || !isDescriptionLong
+                  ? 'collection-description open'
+                  : 'collection-description closed'
+              }
+              dangerouslySetInnerHTML={{ __html: sanitize(description) }}
+            />
+          )}
+          {isDescriptionLong && (
+            <IconButton
+              className='icon-button'
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              <Icon className='icon'>{isOpen ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</Icon>
+            </IconButton>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+const EditableArchiveInfo = ({
+  initialDescription,
+  archiveId,
+}: {
+  initialDescription: string;
+  archiveId: string;
+}) => {
+  const description = useRef<string>(initialDescription);
+
+  const [updateArchive] = useUpdateArchiveMutation({
+    refetchQueries: ['getArchive'],
+  });
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    description.current = initialDescription;
+  }, [initialDescription]);
+
+  const extraOptions = useMemo(
+    () => ({
+      readonly: false,
+      showPlaceholder: true,
+      placeholder: t('curator.insertDescriptionHere'),
+    }),
+    [t]
+  );
+
+  const onBlur = useCallback(() => {
+    // Save description
+    updateArchive({
+      variables: {
+        archiveId,
+        data: {
+          description: description.current,
+        },
+      },
+    });
+  }, [description, archiveId, updateArchive]);
+
+  return (
+    <Editor
+      value={description.current}
+      extraOptions={extraOptions}
+      onBlur={onBlur}
+      onChange={newText => {
+        description.current = newText;
+      }}
+    />
+  );
+};
+
+export default ArchiveInfo;
