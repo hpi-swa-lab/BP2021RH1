33ec3173c8294d8a674cb76f3aa4e19b5ca4dc46
Oliver Schulz <64468276+olschulz@users.noreply.github.com>
2022-12-13 14:21:23 +0100

    eingabefeld font (#183)
    * major changes to text editor (#158)
    
    * fix lint errors
    
    * fix duplication bug
    
    * fix lint errors
    
    * small code cleanup
    
    * remove weird bugging enter button and replace all jodit-react instances
    
    * update jodit-react package to finally be able to access jodit instance and fix prettier bugs
    
    * get rid of unneeded files and ignores
    
    * move default config to component and add comments
    
    Co-authored-by: MariusDoe <41512063+MariusDoe@users.noreply.github.com>

---
+++ projects/bp-gallery/src/components/common/editor/helpers/jodit-config.ts
@@ -0,0 +1,17 @@
+import { Jodit } from 'jodit-react';
+import i18n from '../../../../i18n';
+
+type EnterMode = 'br' | 'div' | 'p';
+
+/** Default configuration options for the editor. See {@link https://xdsoft.net/jodit/docs/classes/config.Config.html} */
+const defaultJoditConfig = {
+  preset: 'inline',
+  enter: 'br' as EnterMode, //Not 'P' to avoid addition of <p> to descriptions
+  askBeforePasteHTML: false,
+  askBeforePasteFromWord: false,
+  useNativeTooltip: true,
+  language: i18n.resolvedLanguage,
+  disablePlugins: ['about', 'fullsize', 'classSpan', 'addNewLine'],
+} as Partial<Jodit['options']>;
+
+export default defaultJoditConfig;
