6b26d6f19a87de1801c991f82a4c631ea43499b4
Oliver Schulz <64468276+olschulz@users.noreply.github.com>
2022-12-14 22:13:01 +0100

    fix description editor focus bug (#198)
    * fix the bug
    
    * use useMemo instead

--- projects/bp-gallery/src/components/common/editor/helpers/jodit-config.ts
+++ projects/bp-gallery/src/components/common/editor/helpers/jodit-config.ts
@@ -4,7 +4,7 @@ import i18n from '../../../../i18n';
 type EnterMode = 'br' | 'div' | 'p';
 
 /** Default configuration options for the editor. See {@link https://xdsoft.net/jodit/docs/classes/config.Config.html} */
-const defaultJoditConfig = {
+const defaultConfig = {
   preset: 'inline',
   enter: 'br' as EnterMode, //Not 'P' to avoid addition of <p> to descriptions
   askBeforePasteHTML: false,
@@ -14,4 +14,4 @@ const defaultJoditConfig = {
   disablePlugins: ['about', 'fullsize', 'classSpan', 'addNewLine'],
 } as Partial<Jodit['options']>;
 
-export default defaultJoditConfig;
+export default defaultConfig;
