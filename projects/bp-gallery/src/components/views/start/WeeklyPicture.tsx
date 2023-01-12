870f3af3f02dbd7a1c0947ff4544ffd7af4fe4da
simon-pierrekrautkramer <skraemer60@gmail.com>
2023-01-12 14:30:48 +0100

    show weekly picture in picture preview and add support for picture view

--- projects/bp-gallery/src/components/views/start/WeeklyPicture.tsx
+++ projects/bp-gallery/src/components/views/start/WeeklyPicture.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useGetPictureInfoQuery } from '../../../graphql/APIConnector';
 import { useSimplifiedQueryResponseData } from '../../../graphql/queryUtils';
 import { FlatPicture } from '../../../types/additionalFlatTypes';
-import { asApiPath } from '../../App';
+import PicturePreview from '../../common/picture-gallery/PicturePreview';
+import {
+  zoomIntoPicture,
+  zoomOutOfPicture,
+} from '../../common/picture-gallery/helpers/picture-animations';
+import PictureView from '../picture/PictureView';
+import { Card } from '@mui/material';
 
 const choosePictureId = (pictureIds: string[]) => {
   const currentDate = new Date();
@@ -13,25 +19,46 @@ const choosePictureId = (pictureIds: string[]) => {
   return pictureIds[resultIndex];
 };
 
-const getPictureLink = (picture: FlatPicture | undefined, fallbackPicturePath: string) => {
-  if (!picture || !picture.media?.url) {
-    return fallbackPicturePath;
-  }
-  return asApiPath(`${picture.media.url}?updatedAt=${picture.media.updatedAt as string}`);
-};
 const WeeklyPicture = () => {
   //These are hard-coded until we implemented votes
-  const fallbackPicturePath = '/bad-harzburg-stiftung-logo.png';
   const pictureIds = ['4'];
-
+  const [isFocussed, setIsFocussed] = useState<boolean>(false);
+  const navigateToPicture = useCallback(
+    async (id: string) => {
+      window.history.pushState({}, '', `/picture/${id}`);
+      await zoomIntoPicture(`picture-preview-for-${id}`);
+      setIsFocussed(true);
+    },
+    [setIsFocussed]
+  );
   const weeklyPictureId = choosePictureId(pictureIds);
   const { data } = useGetPictureInfoQuery({ variables: { pictureId: weeklyPictureId } });
   const picture: FlatPicture | undefined = useSimplifiedQueryResponseData(data)?.picture;
-  console.log(picture);
-  // console.log(picture)
-  const pictureLink = getPictureLink(picture, fallbackPicturePath);
 
-  return <div>{picture && <img src={pictureLink} alt={picture.id} />}</div>;
+  return (
+    <div>
+      {picture && (
+        <Card
+          className={'flex flex-row'}
+          onClick={async () => {
+            await navigateToPicture(picture.id);
+          }}
+        >
+          <p className={'text-blue-600'}>hello</p>
+          <PicturePreview picture={picture} onClick={() => {}} />
+        </Card>
+      )}
+      {isFocussed && picture && (
+        <PictureView
+          initialPictureId={picture.id}
+          onBack={async () => {
+            setIsFocussed(false);
+            await zoomOutOfPicture(`picture-preview-for-${picture.id}`);
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default WeeklyPicture;
