9cbe1d6b35e8d42061e927538c779a640af87a62
baerlach-git <83013270+baerlach-git@users.noreply.github.com>
2023-01-24 22:05:16 +0100

    contact form is working with basic mail-template, more elaborate templates should be possible if wanted, but for now this should do

--- projects/bp-gallery/src/components/views/contact/ContactFormView.tsx
+++ projects/bp-gallery/src/components/views/contact/ContactFormView.tsx
@@ -3,50 +3,64 @@ import './ContactFormView.scss';
 import { useTranslation } from 'react-i18next';
 
 const ContactFormView = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
+
+    const origin: string = window.location.origin
+    let apiPath:string
+    const regex = RegExp('localhost')
+    if (regex.exec(origin)){
+        apiPath = "http://localhost:9000/api/contact"
+    }
+    else{
+        apiPath = "https://harz-history/api/contact"
+    }
+    
   return (
     <div className='contact-form-container'>
-        <h2>
+        <h1>
         {t('contact-form.title')}
-        </h2>
-        <form className='contact-form'>
+        </h1>
+        <form action={apiPath} method='post' encType='multipart/form-data' target="" className='contact-form' >
             <div className='form-contents'>
                 <p>
                     <label>
                         {t('contact-form.choose-archive-label')}
-                        <select>
-                            <option value='j.schueler92@gmx.de'>Test</option>
-                            <option value='bcpresse@gmail.com'>Herbert-Ahrens-Archiv</option>
+                        <select name="recipient">
+                            <option value='Test'>Test</option>
+                            <option value='Herbert-Ahrens-Archiv'>Herbert-Ahrens-Archiv</option>
                         </select>
                     </label>
                 </p>
                 <p>
                     <label>
                         {t('contact-form.name-label')}
-                        <input className = 'form-input name-input' type = 'text'/>
+                        <input name="sender_name" className = 'form-input name-input' type = 'text'/>
                     </label>
                 </p>
                 <p>
                     <label>
                         {t('contact-form.email-label')}
-                        <input className = 'form-input email-input' type = 'email'/>
+                        <input name="email" className = 'form-input email-input' type = 'email'/>
                     </label>
                 </p>
                 <p>
                     <label>
                         {t('contact-form.subject-label')}
-                        <input className = 'form-input subject-input' type = 'email'/>
+                        <input name="subject" className = 'form-input subject-input'/>
                     </label>
                 </p>
                 <p>
                     <label>
                         {t('contact-form.message-label')}
-                        <textarea className='form-input message-input'/>
+                        <textarea name="message" className='form-input message-input'/>
                     </label>
                 </p>
             </div>    
             <div className='submit-button-container'>
-                <input className='submit-input' type ='submit' value={t('contact-form.submit-button-label').toString()}/>
+                <button className='submit-input' type ='submit'>
+                    {t('contact-form.submit-button-label').toString()}
+                </button>
+                
             </div>
         </form>
     </div>
--- projects/bp-strapi/config/plugins.js
+++ projects/bp-strapi/config/plugins.js
@@ -32,5 +32,23 @@ module.exports = ({ env }) => ({
     config: {
       dsn: env('SENTRY_DSN'),
     },
-  }
+  },
+  email: {
+    config: {
+      provider: env('EMAIL_PROVIDER'), // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST'),
+        port: env('EMAIL_SMTP_PORT'),
+        auth: {
+          user: env('EMAIL_SMTP_USER'),
+          pass: env('EMAIL_SMTP_PASS'),
+        },
+      },
+      settings: {
+        defaultFrom: env('EMAIL_ADDRESS_FROM'),
+        defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
+        testAddress: env('TEST_ADRESS')
+      },
+    },
+  },
 });
