674cfd438beb233f52639680f2e1e1ce8055bb1c
ole1711 <42774821+ole1711@users.noreply.github.com>
2023-01-24 16:22:33 +0100

    add ; bc linter complained

--- projects/bp-gallery/cypress/e2e/picture.cy.ts
+++ projects/bp-gallery/cypress/e2e/picture.cy.ts
@@ -55,5 +55,5 @@ describe('Picture View called via link as a curator', () => {
     cy.get('.date-range-picker .MuiCheckbox-root').click();
     cy.get('.date-range-picker .MuiCheckbox-root:not(.Mui-checked)');
     cy.get('.date-range-picker .MuiCheckbox-root').click().type('{esc}');
-  })
+  });
 });
