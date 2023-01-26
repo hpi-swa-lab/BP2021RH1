b0546506e29922190273ef872d685d6f636dd384
Marius Dörbandt <github.marius@doerbandt.de>
2023-01-26 17:26:46 +0100

    Add and improve link-pictures-with-texts tests

--- projects/bp-gallery/cypress/e2e/link-pictures-with-texts.cy.ts
+++ projects/bp-gallery/cypress/e2e/link-pictures-with-texts.cy.ts
@@ -19,7 +19,7 @@ describe('link pictures with texts', () => {
     cy.contains('Verlinkte Bilder');
   });
 
-  it('copy a link to a picture', () => {
+  it('link a picture to a text', () => {
     cy.visit('/picture/2');
     cy.get('.clipboard-editor-open').click();
     cy.contains('.clipboard-editor', 'Keine Bilder');
@@ -33,6 +33,9 @@ describe('link pictures with texts', () => {
       'Ein kopiertes Bild einfügen'
     ).click();
     cy.get('.picture-info-field[data-type="links"] #picture-preview-for-2');
+
+    cy.visit('/picture/2');
+    cy.get('.picture-info-field[data-type="links"] #picture-preview-for-1');
   });
 
   it('clipboard is cleared in new session', () => {
@@ -44,13 +47,40 @@ describe('link pictures with texts', () => {
     cy.contains('.clipboard-editor', 'Keine Bilder');
   });
 
+  it('link a text to a picture', () => {
+    cy.visit('/picture/1');
+    cy.get('.clipboard-editor-open').click();
+    cy.contains('.clipboard-editor', 'Keine Bilder');
+    cy.contains('.clipboard-editor button', 'Aktuellen Text kopieren').click();
+    cy.get('.clipboard-editor #picture-preview-for-1');
+    cy.contains('.clipboard-editor', 'Keine Bilder').should('not.exist');
+
+    cy.visit('/picture/3');
+    cy.contains(
+      '.picture-info-field[data-type="links"] button',
+      'Einen kopierten Text einfügen'
+    ).click();
+    cy.get('.picture-info-field[data-type="links"] #picture-preview-for-1');
+
+    cy.visit('/picture/1');
+    cy.get('.picture-info-field[data-type="links"] #picture-preview-for-3');
+  });
+
   it('cleanup', () => {
     cy.visit('/picture/1');
+    cy.get('.clipboard-editor-open').click();
+    cy.contains('.clipboard-editor button', 'Ablage leeren').click();
+    cy.contains('.clipboard-editor', 'Keine Bilder');
+    cy.get('.picture-info-field[data-type="links"] .picture-preview');
     cy.contains('.picture-info-field[data-type="links"] button', 'Alle Links entfernen').click();
     cy.get('button').contains('Bestätigen').click();
     cy.get('.picture-info-field[data-type="links"] .picture-preview').should('not.exist');
     cy.contains('Als Text markieren').click();
     cy.contains('button', 'Als Text markieren').find('[data-testid="CheckBoxOutlineBlankIcon"]');
     cy.contains('Verlinkte Texte');
+    cy.visit('/picture/2');
+    cy.get('.picture-info-field[data-type="links"] .picture-preview').should('not.exist');
+    cy.visit('/picture/3');
+    cy.get('.picture-info-field[data-type="links"] .picture-preview').should('not.exist');
   });
 });
