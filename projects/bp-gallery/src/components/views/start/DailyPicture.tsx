8055c2ae6bac9b201bd665e0fac35bf0f732e64e
Simon-Pierre Krautkrämer <skraemer60@gmail.com>
2023-01-28 17:28:23 +0100

    Bild des tages (#224)
    * add WeeklyPicture.tsx with functioning picture selection and picture View (but not in Picture Preview)
    
    * Redo navigation menu autoclose (#211)
    
    * install basic tailwind configuration (#218)
    
    * 182 validierungs interfaces für tags (#219)
    
    * implement interface to validate location and keyword tags (locale strings are WIP)
    
    * adjust localization strings and remove unneeded attributes
    
    * rename tag attribute "root" to "visible"
    
    * fix grid css
    
    * remove console log
    
    * show weekly picture in picture preview and add support for picture view
    
    * add basic card with parameters
    
    * add reverse on mobile, link to archive and centered card
    
    * add other files
    
    * trying to fix linter
    
    * just delete the test
    
    * add Portal, changed to picture of the day, changed design to look cleaner
    
    * add Portal, changed to picture of the day, changed design to look cleaner
    
    * Update projects/bp-gallery/src/components/views/start/DailyPicture.tsx
    
    Co-authored-by: David Schroschk <45777834+DavidSchroschk@users.noreply.github.com>
    
    * Renamed some variables
    
    Co-authored-by: David Schroschk <45777834+DavidSchroschk@users.noreply.github.com>
    
    * try to fix tests
    
    * try to fix tests
    
    ---------
    
    Co-authored-by: MariusDoe <41512063+MariusDoe@users.noreply.github.com>
    Co-authored-by: Oliver Schulz <64468276+olschulz@users.noreply.github.com>
    Co-authored-by: David Schroschk <45777834+DavidSchroschk@users.noreply.github.com>

---
+++ projects/bp-gallery/src/components/views/start/DailyPicture.tsx
@@ -0,0 +1,121 @@
+import React, { useCallback, useState } from 'react';
+import { useGetPictureInfoQuery } from '../../../graphql/APIConnector';
+import { useSimplifiedQueryResponseData } from '../../../graphql/queryUtils';
+import { FlatPicture } from '../../../types/additionalFlatTypes';
+import {
+  zoomIntoPicture,
+  zoomOutOfPicture,
+} from '../../common/picture-gallery/helpers/picture-animations';
+import PictureView from '../picture/PictureView';
+import { Portal } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import EventIcon from '@mui/icons-material/Event';
+import { formatTimeStamp } from '../../../helpers/format-timestamp';
+import Editor from '../../common/editor/Editor';
+import { FolderSpecial } from '@mui/icons-material';
+import { asApiPath } from '../../App';
+
+const choosePictureId = (pictureIds: string[]) => {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const days = Math.floor((currentDate.valueOf() - startDate.valueOf()) / (24 * 60 * 60 * 1000));
+  const resultIndex = days % pictureIds.length;
+  return pictureIds[resultIndex];
+};
+
+const DailyPicture = () => {
+  const root = document.getElementById('root');
+  //These are hard-coded until we implemented votes
+  const pictureIds = [
+    '2254',
+    '2265',
+    '11715',
+    '13258',
+    '14440',
+    '13282',
+    '14998',
+    '13124',
+    '13125',
+    '13034',
+    '12689',
+    '12939',
+    '6837',
+    '7943',
+    '6863',
+    '6773',
+    '7886',
+    '7954',
+    '9350',
+    '8255',
+    '10838',
+  ];
+  const { t } = useTranslation();
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const navigateToPicture = useCallback(
+    async (id: string) => {
+      window.history.pushState({}, '', `/picture/${id}`);
+      await zoomIntoPicture(`picture-preview-for-${id}`);
+      setIsFocused(true);
+    },
+    [setIsFocused]
+  );
+  const dailyPictureId = choosePictureId(pictureIds);
+  const { data } = useGetPictureInfoQuery({ variables: { pictureId: dailyPictureId } });
+  const picture: FlatPicture | undefined = useSimplifiedQueryResponseData(data)?.picture;
+  const pictureLink = picture?.media?.url
+    ? asApiPath(`${picture.media.url}?updatedAt=${picture.media.updatedAt as string}`)
+    : '';
+  const description = picture?.descriptions?.[0]?.text || '';
+  const pictureDate = formatTimeStamp(picture?.time_range_tag);
+  const pictureArchive = picture?.archive_tag?.name;
+  const pictureArchiveId = picture?.archive_tag?.id;
+  const pictureArchiveLink = pictureArchiveId ? `/archives/${pictureArchiveId}` : '';
+  return (
+    <div>
+      {picture && (
+        <>
+          <h3 className={'text-2xl'}>{t('common.daily-picture')}</h3>
+          <div className={'flex flex-col place-items-center'}>
+            <img
+              className={'w-screen sm:w-auto sm:h-50vh'}
+              src={pictureLink}
+              alt={t('common.daily-picture')}
+              onClick={async () => {
+                await navigateToPicture(picture.id);
+              }}
+            />
+            <div className={'flex flex-col max-w-4xl'}>
+              <p className={'line-clamp-5'}>
+                <h4 className={'text-lg my-1'}>{t('pictureFields.descriptions')}:</h4>
+                <Editor value={description} />
+              </p>
+              <div className={'flex-1'} />
+              <div className={'flex items-center gap-2 my-2'}>
+                <EventIcon /> {pictureDate}
+              </div>
+              <div className={'flex item-center gap-2'}>
+                <FolderSpecial /> <a href={pictureArchiveLink}>{pictureArchive} </a>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
+      {isFocused && picture && (
+        <Portal container={root}>
+          <PictureView
+            initialPictureId={picture.id}
+            onBack={async () => {
+              setIsFocused(false);
+              await zoomOutOfPicture(`picture-preview-for-${picture.id}`);
+            }}
+          />
+        </Portal>
+      )}
+    </div>
+  );
+};
+
+export default DailyPicture;
+
+export { choosePictureId };
