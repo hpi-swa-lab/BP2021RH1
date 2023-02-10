ce25dd0c1f5af679fd3620ffae6d37b40561cdc8
Marius Dörbandt <github.marius@doerbandt.de>
2023-02-10 15:06:25 +0100

    Memoize intermediate values

--- projects/bp-gallery/src/hooks/get-pictures.hook.ts
+++ projects/bp-gallery/src/hooks/get-pictures.hook.ts
@@ -7,6 +7,7 @@ import {
 } from '../graphql/APIConnector';
 import { NUMBER_OF_PICTURES_LOADED_PER_FETCH } from '../components/common/picture-gallery/PictureScrollGrid';
 import { AuthRole, useAuth } from '../components/provider/AuthProvider';
+import { useCallback, useMemo } from 'react';
 
 const useGetPictures = (
   queryParams: PictureFiltersInput | { searchTerms: string[]; searchTimes: string[][] },
@@ -43,28 +44,42 @@ const useGetPictures = (
   const doFilterOutTexts = role < AuthRole.CURATOR && filterOutTextsForNonCurators;
 
   type PictureData = NonNullable<GetPicturesQuery['pictures']>['data'][number];
-  const filterOutTexts = (pictures: (PictureData | null | undefined)[] | null | undefined) => {
-    if (!pictures) {
-      return undefined;
-    }
-    return {
-      pictures: {
-        data: doFilterOutTexts
-          ? pictures.filter(picture => !picture?.attributes?.is_text)
-          : pictures,
-      },
-    };
-  };
+
+  const filterOutTexts = useCallback(
+    (pictures: (PictureData | null | undefined)[] | null | undefined) => {
+      if (!pictures) {
+        return undefined;
+      }
+      return {
+        pictures: {
+          data: doFilterOutTexts
+            ? pictures.filter(picture => !picture?.attributes?.is_text)
+            : pictures,
+        },
+      };
+    },
+    [doFilterOutTexts]
+  );
+
+  const filteredAllSearchData = useMemo(
+    () => filterOutTexts(customQueryResult.data?.findPicturesByAllSearch),
+    [filterOutTexts, customQueryResult]
+  );
+
+  const filteredQueryData = useMemo(
+    () => filterOutTexts(queryResult.data?.pictures?.data),
+    [filterOutTexts, queryResult]
+  );
 
   if (isAllSearchActive) {
     return {
       ...customQueryResult,
-      data: filterOutTexts(customQueryResult.data?.findPicturesByAllSearch),
+      data: filteredAllSearchData,
     };
   } else {
     return {
       ...queryResult,
-      data: filterOutTexts(queryResult.data?.pictures?.data),
+      data: filteredQueryData,
     };
   }
 };
