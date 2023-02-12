a79ecf0f9c9669174408b54c84e9e19fe84e68c7
simon-pierrekrautkramer <skraemer60@gmail.com>
2023-02-12 13:14:00 +0100

    add new pictures for DailyPicture.tsx, add Card design, fixed transitioning for PictureGrid.tsx

--- projects/bp-gallery/src/components/views/start/DailyPicture.tsx
+++ projects/bp-gallery/src/components/views/start/DailyPicture.tsx
@@ -2,17 +2,14 @@ import { useCallback, useState } from 'react';
 import { useGetPictureInfoQuery } from '../../../graphql/APIConnector';
 import { useSimplifiedQueryResponseData } from '../../../graphql/queryUtils';
 import { FlatPicture } from '../../../types/additionalFlatTypes';
-import {
-  zoomIntoPicture,
-  zoomOutOfPicture,
-} from '../../common/picture-gallery/helpers/picture-animations';
-import PictureView from '../picture/PictureView';
-import { Portal } from '@mui/material';
+import { Card, Portal } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import EventIcon from '@mui/icons-material/Event';
 import { formatTimeStamp } from '../../../helpers/format-timestamp';
 import Editor from '../../common/editor/Editor';
-import { FolderSpecial, Event } from '@mui/icons-material';
+import { FolderSpecial } from '@mui/icons-material';
 import { asApiPath } from '../../../helpers/app-helpers';
+import PictureView from '../picture/PictureView';
 
 const choosePictureId = (pictureIds: string[]) => {
   const currentDate = new Date();
@@ -47,13 +44,30 @@ const DailyPicture = () => {
     '9350',
     '8255',
     '10838',
+    '16393',
+    '16297',
+    '16280',
+    '16235',
+    '16150',
+    '16155',
+    '16134',
+    '16111',
+    '16055',
+    '15941',
+    '15903',
+    '16373',
+    '12625',
+    '14365',
+    '13245',
+    '13116',
+    '13110',
+    '14064',
   ];
   const { t } = useTranslation();
   const [isFocused, setIsFocused] = useState<boolean>(false);
   const navigateToPicture = useCallback(
     async (id: string) => {
       window.history.pushState({}, '', `/picture/${id}`);
-      await zoomIntoPicture(`picture-preview-for-${id}`);
       setIsFocused(true);
     },
     [setIsFocused]
@@ -72,32 +86,33 @@ const DailyPicture = () => {
   return (
     <div>
       {picture && (
-        <>
-          <h3 className={'text-2xl'}>{t('common.daily-picture')}</h3>
-          <div className={'flex flex-col place-items-center'}>
-            <img
-              className={'w-screen sm:w-auto sm:h-50vh'}
-              src={pictureLink}
-              alt={t('common.daily-picture')}
-              onClick={async () => {
-                await navigateToPicture(picture.id);
-              }}
-            />
-            <div className={'flex flex-col max-w-4xl'}>
+        <div className='flex justify-center'>
+          <Card className='flex flex-col-reverse md:flex-row rounded-md justify-between max-w-4xl max-h-fit'>
+            <div className='p-4 flex flex-col'>
+              <h3 className={'text-2xl'}>{t('common.daily-picture')}</h3>
               <p className={'line-clamp-5'}>
                 <h4 className={'text-lg my-1'}>{t('pictureFields.descriptions')}:</h4>
                 <Editor value={description} />
               </p>
               <div className={'flex-1'} />
               <div className={'flex items-center gap-2 my-2'}>
-                <Event /> {pictureDate}
+                <EventIcon /> {pictureDate}
               </div>
               <div className={'flex item-center gap-2'}>
                 <FolderSpecial /> <a href={pictureArchiveLink}>{pictureArchive} </a>
               </div>
             </div>
-          </div>
-        </>
+            <img
+              className={'w-screen sm:w-auto sm:h-96'}
+              id='daily-picture'
+              src={pictureLink}
+              alt={t('common.daily-picture')}
+              onClick={async () => {
+                await navigateToPicture(picture.id);
+              }}
+            />
+          </Card>
+        </div>
       )}
 
       {isFocused && picture && (
@@ -106,7 +121,6 @@ const DailyPicture = () => {
             initialPictureId={picture.id}
             onBack={async () => {
               setIsFocused(false);
-              await zoomOutOfPicture(`picture-preview-for-${picture.id}`);
             }}
           />
         </Portal>
