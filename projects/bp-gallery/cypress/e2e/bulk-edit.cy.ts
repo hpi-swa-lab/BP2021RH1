bc794821d6c7c1f6807cb4062fa37278d83cb3b1
Marius Dörbandt <github.marius@doerbandt.de>
2023-02-14 16:44:25 +0100

    Add bulk edit descriptions tests

--- projects/bp-gallery/cypress/e2e/bulk-edit.cy.ts
+++ projects/bp-gallery/cypress/e2e/bulk-edit.cy.ts
@@ -88,3 +88,81 @@ describe('bulk edit dates', () => {
     cy.contains('01.01.1961 - 31.01.2022');
   });
 });
+
+describe('bulk edit descriptions', () => {
+  before(() => {
+    cy.visit('/');
+    login();
+  });
+  after(() => {
+    cy.visit('/');
+    logout();
+  });
+
+  it('shows the same description as the description', () => {
+    cy.visit('/archives/1');
+    selectPictures('4', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Yet another description');
+  });
+
+  it('shows different descriptions as no description', () => {
+    cy.visit('/archives/1');
+    selectPictures('2', '3');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Keine Beschreibung vorhanden');
+  });
+
+  it('shows only the shared descriptions', () => {
+    cy.visit('/archives/1');
+    selectPictures('1', '2');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Yet another description');
+    cy.contains('This is a description!').should('not.exist');
+  });
+
+  it('adds and deletes descriptions while leaving exclusive descriptions as they are', () => {
+    cy.visit('/archives/1');
+    selectPictures('1', '2');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.picture-info-field', 'Beschreibungen')
+      .contains('.MuiIconButton-root', 'add')
+      .click();
+    cy.contains('.picture-info-field', 'Beschreibungen')
+      .find('.jodit-wysiwyg:empty')
+      .type('Irgendwas cooles');
+    cy.contains('.field-title', 'Beschreibungen').click();
+    cy.contains('.save-state', 'Gespeichert.');
+
+    cy.visit('/picture/1');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!');
+    cy.contains('Irgendwas cooles');
+
+    cy.visit('/picture/2');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!').should('not.exist');
+    cy.contains('Irgendwas cooles');
+
+    cy.visit('/archives/1');
+    selectPictures('1', '2');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.description-wrapper', 'Irgendwas cooles')
+      .contains('.MuiIconButton-root', 'delete')
+      .click();
+    cy.contains('Bestätigen').click();
+    cy.contains('.save-state', 'Gespeichert.');
+    cy.contains('Yet another description');
+    cy.contains('Irgendwas cooles').should('not.exist');
+
+    cy.visit('/picture/1');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!');
+    cy.contains('Irgendwas cooles').should('not.exist');
+
+    cy.visit('/picture/2');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!').should('not.exist');
+    cy.contains('Irgendwas cooles').should('not.exist');
+  });
+});
