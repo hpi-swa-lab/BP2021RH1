2c18ddb6cd6f09492aa8f9a83e9faa0860f6c32f
Marius Dörbandt <github.marius@doerbandt.de>
2023-02-14 15:57:34 +0100

    Add bulk edit dates tests

--- projects/bp-gallery/cypress/e2e/bulk-edit.cy.ts
+++ projects/bp-gallery/cypress/e2e/bulk-edit.cy.ts
@@ -1,5 +1,11 @@
 import { login, logout } from '../utils/login-utils';
 
+const selectPictures = (...ids: string[]) => {
+  for (const id of ids) {
+    cy.get(`#picture-preview-for-${id} .adornment[title="Bild auswählen"]`).click();
+  }
+};
+
 describe('bulk edit', () => {
   before(() => {
     cy.visit('/latest/');
@@ -28,5 +34,57 @@ describe('bulk edit', () => {
     cy.contains('.save-state', 'Gespeichert');
     cy.contains('Zurück').click();
     cy.get('.picture-preview .adornment [data-testid="CheckBoxIcon"]').should('not.exist');
+  });
+});
+
+describe('bulk edit dates', () => {
+  before(() => {
+    cy.visit('/');
+    login();
+  });
+  after(() => {
+    cy.visit('/');
+    logout();
+  });
+
+  it('shows the same date as the date', () => {
+    cy.visit('/archives/1');
+    selectPictures('2', '3');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('01.01.1961 - 31.01.2022');
+  });
+
+  it('shows different dates as no date', () => {
+    cy.visit('/archives/1');
+    selectPictures('2', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Keine Zeit bekannt');
+  });
+
+  it('sets the date of different dates to the same, set date', () => {
+    cy.visit('/archives/1');
+    selectPictures('1', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Keine Zeit bekannt').click();
+    cy.get('.rdrDateInput').eq(0).clear();
+    cy.get('.rdrDateInput').eq(0).type('20.04.1969{enter}{esc}');
+    cy.contains('.save-state', 'Gespeichert.');
+    cy.visit('/picture/5');
+    cy.contains('20.04.1969').click();
+    // cleanup
+    cy.get('.rdrDateInput').eq(0).clear();
+    cy.get('.rdrDateInput').eq(0).type('01.04.1970{enter}{esc}');
+    cy.contains('.save-state', 'Gespeichert.');
+    cy.contains('01.04.1970');
+
+    cy.visit('/picture/1');
+    cy.contains('20.04.1969').click();
+    // cleanup
+    cy.get('.rdrDateInput').eq(0).clear();
+    cy.get('.rdrDateInput').eq(0).type('01.01.1961{enter}');
+    cy.get('.rdrDateInput').eq(1).clear();
+    cy.get('.rdrDateInput').eq(1).type('31.01.2022{enter}{esc}');
+    cy.contains('.save-state', 'Gespeichert.');
+    cy.contains('01.01.1961 - 31.01.2022');
   });
 });
