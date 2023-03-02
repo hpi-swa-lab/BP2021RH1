75287ca854ce10ddc5c0938d4ba3bb30a00e5381
Marius Dörbandt <github.marius@doerbandt.de>
2023-03-02 11:00:57 +0100

    Wrap listen call in useEffect

--- projects/bp-gallery/src/matomo-config/TrackHistoryWithMatomo.tsx
+++ projects/bp-gallery/src/matomo-config/TrackHistoryWithMatomo.tsx
@@ -1,12 +1,15 @@
 import { History } from 'history';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { trackHistory } from './matomo';
 
 export const TrackHistoryWithMatomo = ({ children }: PropsWithChildren<{}>) => {
   const history: History = useHistory();
-  history.listen(() => {
-    trackHistory();
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      trackHistory();
+    });
+    return unlisten;
   });
   return <>{children}</>;
 };
