f7a5f89bf924dbeadc8c5122f441776d95143dde
baerlach-git <83013270+baerlach-git@users.noreply.github.com>
2023-03-02 11:17:32 +0100

    201 kontaktformular überarbeiten (#266)
    * added contact form skeleton and german localization files, refactored Browseview and App so that the footer now shows on more pages than just the start page, copy pasted some of the necessary css for this change from a backup of lost progress and made some css adjustments
    
    * fixed the display of the footer, extended the contact form and added css for the contact form
    
    * contact form is working with basic mail-template, more elaborate templates should be possible if wanted, but for now this should do
    
    * removed the Test-option for the recipient selection in the contact form
    
    * hopefully deleted all the footer stuff i changed before
    
    * merged staging into 201 and hopefully removed all previously made footer changes
    
    * removed unneccessary lines and comments from TOS and App Css
    
    * added empty line wanted by linter in app css
    
    * Add missing yarn.lock updates
    
    * fixed prettier and saved all the files to autoformat
    
    * switched from scss to tailwind, autoformated unformated files
    
    * deleted scss file
    
    * turned the button into a Mui-Button and gave it a red background with white text, to make it match other Buttons on the site
    
    * removed the 'Test'-option for sending contact messages
    
    ---------
    
    Co-authored-by: Marius Dörbandt <github.marius@doerbandt.de>
    Co-authored-by: Oliver Schulz <64468276+olschulz@users.noreply.github.com>

---
+++ projects/bp-gallery/src/components/views/contact/ContactFormView.tsx
@@ -0,0 +1,75 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const ContactFormView = () => {
+  const { t } = useTranslation();
+
+  const origin: string = window.location.origin;
+  let apiPath: string;
+  const regex = RegExp('localhost');
+  if (regex.exec(origin)) {
+    apiPath = 'http://localhost:9000/api/contact';
+  } else {
+    apiPath = 'https://harz-history/api/contact';
+  }
+
+  return (
+    <div className='contact-form-container flex flex-col flex-nowrap items-center m-auto p-4'>
+      <h1 className='pt-8'>{t('contact-form.title')}</h1>
+      <form
+        action={apiPath}
+        method='post'
+        encType='multipart/form-data'
+        target=''
+        className='contact-form w-fit h-fit'
+      >
+        <div className='form-contents'>
+          <p>
+            <label className='flex flex-col flex-nowrap text-xl p-0'>
+              {t('contact-form.choose-archive-label')}
+              <select className='max-w h-6' name='recipient'>
+                {/*    <option value='Test'>Test</option> */}
+                <option value='Herbert-Ahrens-Archiv'>Herbert-Ahrens-Archiv</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label className='flex flex-col flex-nowrap text-xl p-0'>
+              {t('contact-form.name-label')}
+              <input name='sender_name' className='form-input h-5 w-80 name-input' type='text' />
+            </label>
+          </p>
+          <p>
+            <label className='flex flex-col flex-nowrap text-xl p-0'>
+              {t('contact-form.email-label')}
+              <input name='email' className='form-input h-5 w-80 email-input' type='email' />
+            </label>
+          </p>
+          <p>
+            <label className='flex flex-col flex-nowrap text-xl p-0'>
+              {t('contact-form.subject-label')}
+              <input name='subject' className='form-input h-5 w-80 subject-input' />
+            </label>
+          </p>
+          <p>
+            <label className='flex flex-col flex-nowrap text-xl p-0'>
+              {t('contact-form.message-label')}
+              <textarea name='message' className='form-input max-w message-input h-20' />
+            </label>
+          </p>
+          <div className='submit-button-container '></div>
+          <Button
+            className='submit-input'
+            sx={{ 'background-color': '#7e241d', color: 'white' }}
+            type='submit'
+          >
+            {t('contact-form.submit-button-label').toString()}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactFormView;
--- projects/bp-strapi/config/plugins.js
+++ projects/bp-strapi/config/plugins.js
@@ -28,9 +28,27 @@ module.exports = ({ env }) => ({
     },
   },
   sentry: {
-    enabled: !!env('SENTRY_DSN'),
+    enabled: !!env("SENTRY_DSN"),
     config: {
-      dsn: env('SENTRY_DSN'),
+      dsn: env("SENTRY_DSN"),
     },
-  }
+  },
+  email: {
+    config: {
+      provider: env("EMAIL_PROVIDER"),
+      providerOptions: {
+        host: env("EMAIL_SMTP_HOST"),
+        port: env("EMAIL_SMTP_PORT"),
+        auth: {
+          user: env("EMAIL_SMTP_USER"),
+          pass: env("EMAIL_SMTP_PASS"),
+        },
+      },
+      settings: {
+        defaultFrom: env("EMAIL_ADDRESS_FROM"),
+        defaultReplyTo: env("EMAIL_ADDRESS_REPLY"),
+        testAddress: env("TEST_ADDRESS"),
+      },
+    },
+  },
 });
