d482fa4d35fdc432988fe4ea00cefe29b2a0f500
Marius Dörbandt <github.marius@doerbandt.de>
2023-03-06 18:47:11 +0100

    Remove unnecessary login

--- projects/bp-gallery/cypress/e2e/picture-grid.cy.ts
+++ projects/bp-gallery/cypress/e2e/picture-grid.cy.ts
@@ -1,15 +1,8 @@
 import { login, logout } from '../utils/login-utils';
 
 describe('picture grid', () => {
-  before(() => {
-    cy.visit('/start');
-    login();
-  });
-  after(() => {
-    logout();
-  });
-
   it('should render more pictures after scrolling', () => {
+    cy.visit('/start');
     cy.contains('.picture-count', 'Mehr als 100 Bilder');
     cy.get('.picture-grid .picture-preview').should('have.length', 100);
     cy.get('.App > .scroll-context > .scrollable-container').scrollTo('bottom');
