9131688f65499908820b6fcb30887e4a30d83b5a
Marius Dörbandt <github.marius@doerbandt.de>
2023-03-06 18:29:56 +0100

    Fix back button after navigation

--- projects/bp-gallery/src/helpers/history.ts
+++ projects/bp-gallery/src/helpers/history.ts
@@ -4,3 +4,8 @@ export const pushHistoryWithoutRouter = (newLocation: string) => {
   window.history.pushState({}, '', newLocation);
   trackHistory();
 };
+
+export const replaceHistoryWithoutRouter = (newLocation: string) => {
+  window.history.replaceState({}, '', newLocation);
+  trackHistory();
+};
