d16dce64e9cb47398d75b0666d71863b08f7255a
Marius Dörbandt <github.marius@doerbandt.de>
2023-03-07 10:22:51 +0100

    Add bulk edit keyword tags

---
+++ projects/bp-gallery/cypress/e2e/bulk-edit/keyword-tags.cy.ts
@@ -0,0 +1,114 @@
+import { login, logout } from '../../utils/login-utils';
+import { selectPictures } from './helper';
+
+describe('bulk edit keyword tags', () => {
+  before(() => {
+    cy.visit('/');
+    login();
+  });
+  after(() => {
+    cy.visit('/');
+    logout();
+  });
+
+  it('shows the intersection of the given tags', () => {
+    cy.visit('/picture/4');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/archives/1');
+    selectPictures('3', '4');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+  });
+
+  it("shows no tags when one picture doesn't have any tags", () => {
+    cy.visit('/picture/4');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/5');
+    cy.contains('.picture-info-field', 'Schlagworte').find('input');
+    cy.contains('.picture-info-field', 'Schlagworte').find('.MuiChip-root').should('not.exist');
+    cy.visit('/archives/1');
+    selectPictures('4', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.picture-info-field', 'Schlagworte').find('input');
+    cy.contains('.picture-info-field', 'Schlagworte').find('.MuiChip-root').should('not.exist');
+  });
+
+  it('adds new tags while keeping old ones', () => {
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/picture/5');
+    cy.contains('Verifiziertes Testschlagwort 2').should('not.exist');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/archives/1');
+    selectPictures('3', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.picture-info-field', 'Schlagworte')
+      .find('input')
+      .type('Verifiziertes Testschlagwort 4{enter}');
+    cy.contains('.save-state', 'Gespeichert');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/5');
+    cy.contains('Verifiziertes Testschlagwort 2').should('not.exist');
+    cy.contains('Verifiziertes Testschlagwort 4');
+  });
+
+  it('removes tags while keeping untouched ones', () => {
+    cy.visit('/archives/1');
+    selectPictures('3', '5');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.MuiChip-root', 'Verifiziertes Testschlagwort 4')
+      .find('[data-testid="CancelIcon"]')
+      .click();
+    cy.contains('.save-state', 'Gespeichert');
+    cy.contains('Verifiziertes Testschlagwort 2').should('not.exist');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/picture/5');
+    cy.contains('Verifiziertes Testschlagwort 2').should('not.exist');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+  });
+
+  it("adds tags to pictures which don't have them yet, even if some already have them", () => {
+    cy.visit('/picture/4');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+    cy.visit('/archives/1');
+    selectPictures('3', '4');
+    cy.contains('Mehrere Bilder editieren').click();
+    cy.contains('.picture-info-field', 'Schlagworte')
+      .find('input')
+      .type('Verifiziertes Testschlagwort 4{enter}');
+    cy.contains('.save-state', 'Gespeichert');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/4');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    cy.visit('/picture/3');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4');
+    // cleanup
+    cy.get('[data-testid="CancelIcon"]'); // wait for auth
+    cy.contains('.MuiChip-root', 'Verifiziertes Testschlagwort 4')
+      .find('[data-testid="CancelIcon"]')
+      .click();
+    cy.contains('.save-state', 'Gespeichert');
+    cy.contains('Verifiziertes Testschlagwort 2');
+    cy.contains('Verifiziertes Testschlagwort 4').should('not.exist');
+  });
+});
