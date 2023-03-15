f29261fc520439d6668096b875108c463e05f597
simon-pierrekrautkramer <skraemer60@gmail.com>
2023-03-15 14:51:29 +0100

    show picture from archive

--- projects/bp-gallery/src/components/views/geoguessr/GeoView.tsx
+++ projects/bp-gallery/src/components/views/geoguessr/GeoView.tsx
@@ -1,10 +1,19 @@
 import GeoMap from './GeoMap';
 import './GeoView.scss';
+import { useGetPictureInfoQuery } from '../../../graphql/APIConnector';
+import { FlatPicture } from '../../../types/additionalFlatTypes';
+import { useSimplifiedQueryResponseData } from '../../../graphql/queryUtils';
+import { asApiPath } from '../../../helpers/app-helpers';
 
 const GeoView = () => {
+  const { data } = useGetPictureInfoQuery({ variables: { pictureId: '70' } });
+  const picture: FlatPicture | undefined = useSimplifiedQueryResponseData(data)?.picture;
+  const pictureLink = picture?.media?.url
+    ? asApiPath(`${picture.media.url}?updatedAt=${picture.media.updatedAt as string}`)
+    : '';
   return (
     <div className='map-container'>
-      <img src='/bad-harzburg-stiftung-logo.png' />
+      <img src={pictureLink} />
       <GeoMap />
     </div>
   );
