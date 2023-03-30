636710fee6a2c257aee9c0d3afca3ae01901c3b0
olschulz <64468276+olschulz@users.noreply.github.com>
2023-03-30 12:18:41 +0200

    fix picture stats not being shown correctly in search

--- projects/bp-gallery/src/hooks/get-pictures.hook.ts
+++ projects/bp-gallery/src/hooks/get-pictures.hook.ts
@@ -1,11 +1,11 @@
+import { useMemo } from 'react';
+import { AuthRole, useAuth } from '../components/provider/AuthProvider';
 import {
   GetPicturesByAllSearchQueryVariables,
   PictureFiltersInput,
   useGetPicturesByAllSearchQuery,
   useGetPicturesQuery,
 } from '../graphql/APIConnector';
-import { AuthRole, useAuth } from '../components/provider/AuthProvider';
-import { useMemo } from 'react';
 
 export const NUMBER_OF_PICTURES_LOADED_PER_FETCH = 100;
 
