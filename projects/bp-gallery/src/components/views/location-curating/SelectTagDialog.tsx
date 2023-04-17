949397d113bf0b098bfb08465197692ecea4e228
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2023-04-17 13:23:02 +0200

    collapse multiple paths into Mehrere Pfade instead of showing all (SelectTagDialog)

--- projects/bp-gallery/src/components/views/location-curating/SelectTagDialog.tsx
+++ projects/bp-gallery/src/components/views/location-curating/SelectTagDialog.tsx
@@ -71,30 +71,10 @@ const TagSelectDialogPreset = ({
     return tagSupertags;
   }, [flattenedTags, tagTree]);
 
-  const [tagList, setTagList] = useState<(FlatTag & { appearance?: number })[]>();
+  const [tagList, setTagList] = useState<FlatTag[]>();
 
   useEffect(() => {
-    const tempTagList = [] as (FlatTag & { appearance?: number })[];
-    flattenedTags?.forEach(tag => {
-      if (!tag.parent_tags?.length) {
-        tempTagList.push({ ...tag, appearance: 0 } as FlatTag & { appearance?: number });
-      } else {
-        let index = 0;
-        tag.parent_tags.forEach(parent => {
-          if (!tagSupertagList || !tagSupertagList[parent.id].length) {
-            tempTagList.push({ ...tag, appearance: index } as FlatTag & { appearance?: number });
-            index++;
-          } else {
-            tagSupertagList[parent.id].forEach(path => {
-              tempTagList.push({ ...tag, appearance: index } as FlatTag & { appearance?: number });
-              index++;
-            });
-          }
-        });
-      }
-    });
-
-    setTagList(tempTagList);
+    setTagList(flattenedTags);
   }, [setTagList, flattenedTags, tagSupertagList]);
 
   return (
@@ -105,33 +85,39 @@ const TagSelectDialogPreset = ({
         title: t(`tag-panel.relocate-${dialogProps.type ?? TagType.KEYWORD}`),
       }}
       allOptions={tagList ?? []}
-      renderOption={(props, option: FlatTag & { appearance?: number }) => {
+      renderOption={(props, option: FlatTag) => {
         return (
-          <li
-            {...props}
-            key={
-              option.appearance
-                ? +option.id >= option.appearance
-                  ? +option.id * +option.id + +option.id + option.appearance
-                  : +option.id + option.appearance * option.appearance
-                : option.id
-            }
-          >
+          <li {...props} key={option.id}>
             <div className='recommendation-item-container'>
               {tagSupertagList && tagSupertagList[option.id].length > 0 ? (
-                <div className='recommendation-item-parents'>
-                  {tagSupertagList[option.id][option.appearance ?? 0].map((tag, index) => {
-                    return (
-                      <div key={index} className='recommendation-item'>
-                        {index >= 1 && <ArrowRight />}
-                        {tag.name}
+                <>
+                  {tagSupertagList[option.id].length > 1 ? (
+                    // multiple paths tag
+                    <div className='recommendation-item-parents'>
+                      <div className='recommendation-item-multiple-paths'>
+                        {t('tag-panel.multiple-paths')}
                       </div>
-                    );
-                  })}
-                  {tagSupertagList[option.id].length > 0 && <ArrowRight />}
-                  {option.name}
-                </div>
+                      <ArrowRight />
+                      <div className='recommendation-item-name'>{option.name}</div>
+                    </div>
+                  ) : (
+                    // single path tag
+                    <div className='recommendation-item-parents'>
+                      {tagSupertagList[option.id][0].map((tag, index) => {
+                        return (
+                          <div key={index} className='recommendation-item'>
+                            {index >= 1 && <ArrowRight />}
+                            <div className='recommendation-item-name'>{tag.name}</div>
+                          </div>
+                        );
+                      })}
+                      <ArrowRight />
+                      <div className='recommendation-item-name'>{option.name}</div>
+                    </div>
+                  )}
+                </>
               ) : (
+                // root tag
                 <div className='recommendation-item-name'>{option.name}</div>
               )}
             </div>
