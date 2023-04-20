7f3708cb069e430e91085eab0673daac1767474c
baerlach-git <83013270+baerlach-git@users.noreply.github.com>
2023-04-20 15:54:19 +0200

    Clean up and fix tests

--- projects/bp-gallery/cypress/e2e/facetags.cy.ts
+++ projects/bp-gallery/cypress/e2e/facetags.cy.ts
@@ -9,14 +9,14 @@ describe('face tagging', () => {
 
   after(() => {
     cy.visit('/');
-
     cy.get('.nav-bar').contains('Mehr...').click();
     cy.get('.MuiPaper-root').contains('Personen').click();
     cy.contains('.MuiDataGrid-row', 'TestPerson').find('[data-testid="DeleteIcon"]').click();
     cy.get('.MuiButton-root').contains('Bestätigen').click();
+    cy.contains('.MuiDataGrid-row', 'TestPerson').should('not.exist');
     cy.contains('.MuiDataGrid-row', 'Personentest').find('[data-testid="DeleteIcon"]').click();
     cy.get('.MuiButton-root').contains('Bestätigen').click();
-
+    cy.contains('.MuiDataGrid-row', 'Personentest').should('not.exist');
     cy.visit('/');
     logout();
   });
@@ -39,31 +39,27 @@ describe('face tagging', () => {
     cy.contains('Personentest hinzufügen').click();
     cy.contains('.save-state', 'Gespeichert');
 
-    cy.contains('Personen markieren').should('exist').click();
-    cy.contains('Personen bearbeiten').should('exist');
+    cy.contains('Personen markieren').click();
+    cy.contains('Personen bearbeiten');
 
     cy.contains('TestPerson').click();
-    cy.get('.picture').trigger('mousemove', 'left');
-    cy.contains('lädt').should('not.exist');
-    cy.get('.picture').click('left').click().wait(1000);
-    cy.get('.facetag').contains('TestPerson').should('exist');
+    cy.get('.picture').click('left');
+    cy.contains('.facetag', 'TestPerson').find('[data-testid="CancelIcon"]');
 
     cy.contains('Personentest').click();
-    cy.get('.picture').trigger('mousemove');
-    cy.contains('lädt').should('not.exist');
-    cy.get('.picture').click().click().wait(1000);
-    cy.get('.facetag').contains('Personentest').should('exist');
+    cy.get('.picture').click();
+    cy.contains('.facetag', 'Personentest').find('[data-testid="CancelIcon"]');
   });
 
   it('hide and display facetags', () => {
     cy.contains('Personen bearbeiten').click();
     cy.contains('Personenmarkierungen anzeigen').should('not.exist');
-    cy.contains('Personenmarkierungen ausblenden').should('exist').click();
+    cy.contains('Personenmarkierungen ausblenden').click();
     cy.get('.facetag').should('not.exist');
     cy.contains('Personenmarkierungen ausblenden').should('not.exist');
-    cy.contains('Personenmarkierungen anzeigen').should('exist').click();
+    cy.contains('Personenmarkierungen anzeigen').click();
     cy.get('.facetag').should('have.length', 2);
-    cy.contains('Personenmarkierungen ausblenden').should('exist');
+    cy.contains('Personenmarkierungen ausblenden');
 
     //test wether it works as a regular user too
     cy.visit('/');
@@ -72,41 +68,38 @@ describe('face tagging', () => {
 
     cy.get('.facetag').should('have.length', 2);
 
+    cy.contains('Personen markieren').should('not.exist');
+    cy.contains('Personen bearbeiten').should('not.exist');
     cy.contains('Personenmarkierungen anzeigen').should('not.exist');
-    cy.contains('Personenmarkierungen ausblenden').should('exist').click();
+    cy.contains('Personenmarkierungen ausblenden').click();
     cy.get('.facetag').should('not.exist');
     cy.contains('Personenmarkierungen ausblenden').should('not.exist');
-    cy.contains('Personenmarkierungen anzeigen').should('exist').click();
+    cy.contains('Personenmarkierungen anzeigen').click();
     cy.get('.facetag').should('have.length', 2);
-    cy.contains('Personenmarkierungen ausblenden').should('exist');
+    cy.contains('Personenmarkierungen ausblenden');
   });
 
   it('delete and move facetags', () => {
     cy.visit('/');
     login();
     cy.visit('/picture/1');
-    cy.contains('Personen markieren').should('exist').click();
-    cy.contains('.MuiChip-label', 'TestPerson').should('exist');
-    cy.contains('.facetag', 'TestPerson')
-      .should('exist')
-      .find('[data-testid="CancelIcon"]')
-      .click();
+    cy.contains('Personen markieren').click();
+    cy.contains('.MuiChip-root', 'TestPerson');
+    cy.contains('.facetag', 'TestPerson').find('[data-testid="CancelIcon"]').click();
     cy.contains('.facetag', 'TestPerson').should('not.exist');
 
-    cy.contains('.MuiChip-label', 'Personentest').should('exist');
+    cy.contains('.MuiChip-root', 'Personentest');
     cy.contains('.facetag', 'Personentest').find('[data-testid="OpenWithIcon"]').click();
     cy.contains('.facetag', 'Personentest')
       .find('[data-testid="OpenWithIcon"]')
       .should('not.exist');
-    cy.get('.picture').trigger('movemouse', 'left');
-    cy.contains('lädt').should('not.exist');
-    cy.get('.picture').trigger('movemouse', 'left');
-    cy.get('.picture').click('left').click('left').wait(1000);
-    cy.contains('.facetag', 'Personentest').find('[data-testid="OpenWithIcon"]').should('exist');
+    cy.get('.picture').click('left');
+    cy.contains('.facetag', 'Personentest').find('[data-testid="OpenWithIcon"]');
 
-    cy.contains('.MuiChip-label', 'Personentest');
-    cy.contains('.facetag', 'Personentest').find('[data-testid="CancelIcon"]').click();
-    cy.get('.facetag').contains('Personen').should('not.exist');
-    cy.contains('.MuiChip-label', 'Personentest');
+    cy.contains('Personen bearbeiten').click();
+    cy.contains('.facetag', 'Personentest');
+    cy.contains('.MuiChip-root', 'Personentest').find('[data-testid="CancelIcon"]').click();
+    cy.contains('.MuiChip-root', 'Personentest').should('not.exist');
+    cy.contains('.facetag', 'Personentest').should('not.exist');
   });
 });
