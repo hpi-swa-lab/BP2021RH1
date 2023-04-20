6be1e067d7afa2822e63b13167c8d17f38597c5d
ole1711 <42774821+ole1711@users.noreply.github.com>
2023-04-20 14:26:39 +0200

    setup playwright and wrote first little tests

---
+++ projects/bp-strapi/tests/firstTest.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test';
+test.beforeEach(async ({page}) => {
+    await page.goto("localhost:3000")
+})
+
+test.describe("first tests", () => {
+    test ('has correct title', async ({page}) => {
+        await expect(page).toHaveTitle(/Das Herbert-Ahrens-Bilderarchiv/);
+    });
+    
+    test ("navigates to search", async ({page}) => {
+        await page.getByRole("link", {name: "Suchen"}).click();
+        await expect(page).toHaveURL(/.*search/);
+    })
+})
