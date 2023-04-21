1f8d6d902c62f4257e3cd57445656fa8d9b9e6e8
Marius Dörbandt <github.marius@doerbandt.de>
2023-04-21 22:31:54 +0200

    Fix imageRect calculation

--- projects/bp-gallery/src/components/views/picture/face-tagging/helpers/image-rect.ts
+++ projects/bp-gallery/src/components/views/picture/face-tagging/helpers/image-rect.ts
@@ -4,9 +4,12 @@ export const useImageRect = (img: HTMLImageElement | null) => {
   const computeRect = useCallback((img: HTMLImageElement) => {
     const { x, y, width, height } = img.getBoundingClientRect();
     const { naturalHeight, naturalWidth } = img;
-    const ratio = naturalWidth / naturalHeight;
-    const realWidth = isNaN(ratio) || ratio > 1 ? width : height * ratio;
-    const realHeight = isNaN(ratio) || ratio < 1 ? height : width / ratio;
+    const naturalRatio = naturalWidth / naturalHeight;
+    const containerRatio = width / height;
+    const realWidth =
+      isNaN(naturalRatio) || naturalRatio > containerRatio ? width : height * naturalRatio;
+    const realHeight =
+      isNaN(naturalRatio) || naturalRatio < containerRatio ? height : width / naturalRatio;
     const realX = x + (width - realWidth) / 2;
     const realY = y + (height - realHeight) / 2;
     return {
