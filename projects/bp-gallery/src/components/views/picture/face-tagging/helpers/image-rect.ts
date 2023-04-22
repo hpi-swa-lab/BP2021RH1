c6529e355169c5b2f38809b26a1deaf0f264cdd6
Marius Dörbandt <github.marius@doerbandt.de>
2023-04-22 18:22:29 +0200

    Split useImageRect into separate functions

--- projects/bp-gallery/src/components/views/picture/face-tagging/helpers/image-rect.ts
+++ projects/bp-gallery/src/components/views/picture/face-tagging/helpers/image-rect.ts
@@ -1,25 +1,8 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
-export const useImageRect = (img: HTMLImageElement | null) => {
-  const computeRect = useCallback((img: HTMLImageElement) => {
-    const { x, y, width, height } = img.getBoundingClientRect();
-    const { naturalHeight, naturalWidth } = img;
-    const naturalRatio = naturalWidth / naturalHeight;
-    const containerRatio = width / height;
-    const realWidth =
-      isNaN(naturalRatio) || naturalRatio > containerRatio ? width : height * naturalRatio;
-    const realHeight =
-      isNaN(naturalRatio) || naturalRatio < containerRatio ? height : width / naturalRatio;
-    const realX = x + (width - realWidth) / 2;
-    const realY = y + (height - realHeight) / 2;
-    return {
-      x: realX,
-      y: realY,
-      width: realWidth,
-      height: realHeight,
-    };
-  }, []);
+type Rect = { x: number; y: number; width: number; height: number };
 
+export const useImageRect = (img: HTMLImageElement | null) => {
   const [rect, setRect] = useState(() => (img ? computeRect(img) : null));
 
   const recompute = useCallback(
@@ -34,10 +17,55 @@ export const useImageRect = (img: HTMLImageElement | null) => {
   // Since position: fixed; breaks that hierarchy and
   // PictureView is sometimes positioned like so, we can't
   // just use document.documentElement or similar.
-  // Instead, we walk upwards from img to find the first
-  // ancestor with position: fixed.
-  const root = useMemo(() => {
-    let current: HTMLElement | null = img;
+  // Instead, we use the closest ancestor with position: fixed.
+  const root = useClosestPositionFixedAncestor(img);
+
+  // recompute when img or root changes
+  useEffect(() => {
+    if (!img || !root) {
+      setRect(null);
+      return;
+    }
+    recompute(img);
+  }, [img, root]);
+
+  // observe root too, so the IntersectionObserver below always has correct margins
+  useResizeObserver(img, [root], recompute);
+
+  useStyleObserver(img, recompute);
+
+  // This should be more general than the MutationObserver above
+  // (i. e. it catches position changes which the MutationObserver
+  // doesn't, e. g. the img sliding horizontally because the PictureInfo
+  // is being hidden), but in practice, using a MutationObserver in
+  // addition leads to quicker and thus smoother updates, so we keep it.
+  useIntersectionObserver(img, root, rect, recompute);
+
+  return rect;
+};
+
+const computeRect = (img: HTMLImageElement): Rect => {
+  const { x, y, width, height } = img.getBoundingClientRect();
+  const { naturalHeight, naturalWidth } = img;
+  const naturalRatio = naturalWidth / naturalHeight;
+  const containerRatio = width / height;
+  const realWidth =
+    isNaN(naturalRatio) || naturalRatio > containerRatio ? width : height * naturalRatio;
+  const realHeight =
+    isNaN(naturalRatio) || naturalRatio < containerRatio ? height : width / naturalRatio;
+  const realX = x + (width - realWidth) / 2;
+  const realY = y + (height - realHeight) / 2;
+  return {
+    x: realX,
+    y: realY,
+    width: realWidth,
+    height: realHeight,
+  };
+};
+
+const useClosestPositionFixedAncestor = (child: HTMLElement | null) => {
+  return useMemo(() => {
+    let current = child;
     while (current) {
       if (getComputedStyle(current).position === 'fixed') {
         return current;
@@ -45,71 +73,87 @@ export const useImageRect = (img: HTMLImageElement | null) => {
       current = current.parentElement;
     }
     return null;
-  }, [img]);
+  }, [child]);
+};
 
+const useResizeObserver = <Subject extends HTMLElement>(
+  subject: Subject | null,
+  additionalElements: (HTMLElement | null)[],
+  onChange: (subject: Subject) => void
+) => {
   useEffect(() => {
-    if (!img || !root) {
-      setRect(null);
+    if (!subject) {
       return;
     }
-    recompute(img);
-
-    const resizeObserver = new ResizeObserver(() => {
-      recompute(img);
+    const observer = new ResizeObserver(() => {
+      onChange(subject);
     });
-    resizeObserver.observe(img);
-    // observe root, so the IntersectionObserver below always has correct margins
-    resizeObserver.observe(root);
+    for (const element of [subject, ...additionalElements]) {
+      if (element) {
+        observer.observe(element);
+      }
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [subject, additionalElements, onChange]);
+};
 
-    const mutationObserver = new MutationObserver(() => {
-      recompute(img);
+const useStyleObserver = <Subject extends HTMLElement>(
+  subject: Subject | null,
+  onChange: (subject: Subject) => void
+) => {
+  useEffect(() => {
+    if (!subject) {
+      return;
+    }
+    const observer = new MutationObserver(() => {
+      onChange(subject);
     });
-    mutationObserver.observe(img, {
+    observer.observe(subject, {
       attributeFilter: ['style'],
     });
-
     return () => {
-      resizeObserver.disconnect();
-      mutationObserver.disconnect();
+      observer.disconnect();
     };
-  }, [img, root, recompute]);
+  }, [subject, onChange]);
+};
 
+const useIntersectionObserver = <Subject extends HTMLElement>(
+  subject: Subject | null,
+  root: HTMLElement | null,
+  currentRect: Rect | null,
+  onChange: (subject: Subject) => void
+) => {
   // Use an IntersectionObserver to watch for position changes.
   // This works by constructing an IntersectionObserver that
   // watches for changes in the intersection rectangle of the
-  // img and the rectangle it currently occupies. When the img
+  // subject and the rectangle it currently occupies. When the subject
   // moves, its intersection rectangle with the given rectangle
-  // changes size. Since we provide a threshold of 1 for the
+  // changes size. Since we provide a threshold near 1 for the
   // intersection ratio, a slight change in the size will trigger
   // our given callback.
-  // This should be more general than the MutationObserver above
-  // (i. e. it catches position changes which the MutationObserver
-  // doesn't, e. g. the img sliding horizontally because the PictureInfo
-  // is being hidden), but in practice, using a MutationObserver in
-  // addition leads to quicker and thus smoother updates, so we keep it.
   useEffect(() => {
-    if (!img || !root || !rect) {
+    if (!subject || !root || !currentRect) {
       return;
     }
 
     const rootRect = root.getBoundingClientRect();
-    // can't use imageRect here, since it takes the aspect ratio
-    // into account, which the IntersectionObserver doesn't
-    const imgRect = img.getBoundingClientRect();
+    const subjectRect = subject.getBoundingClientRect();
 
-    // Compute the rectangle that the img currently occupies,
+    // Compute the rectangle that the subject currently occupies,
     // relative to root's rectangle. Negative margins are inwards, so
     // right and bottom are the other way around.
     const rootMargin = [
-      rootRect.top - imgRect.top,
-      imgRect.right - rootRect.right,
-      imgRect.bottom - rootRect.bottom,
-      rootRect.left - imgRect.left,
+      rootRect.top - subjectRect.top,
+      subjectRect.right - rootRect.right,
+      subjectRect.bottom - rootRect.bottom,
+      rootRect.left - subjectRect.left,
     ]
       .map(margin => `${margin}px`)
       .join(' ');
 
-    const { width, height } = imgRect;
+    const { width, height } = subjectRect;
     // account for one pixel error on each side due to subpixel rounding issues
     const onePixelError = (2 * (width + height)) / (width * height);
     const threshold = isFinite(onePixelError) ? 1 - onePixelError : 1;
@@ -121,7 +165,7 @@ export const useImageRect = (img: HTMLImageElement | null) => {
           // change the intersectionRatio
           return;
         }
-        recompute(img);
+        onChange(subject);
       },
       {
         root,
@@ -129,12 +173,10 @@ export const useImageRect = (img: HTMLImageElement | null) => {
         threshold,
       }
     );
-    observer.observe(img);
+    observer.observe(subject);
 
     return () => {
       observer.disconnect();
     };
-  }, [img, root, recompute, rect]);
-
-  return rect;
+  }, [subject, root, currentRect, onChange]);
 };
