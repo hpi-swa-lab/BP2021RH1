1dc035e9a9bdf8d499cc9d990bd05c8afdb71059
Marius Dörbandt <github.marius@doerbandt.de>
2023-04-26 13:22:32 +0200

    Add preconditions for descriptions tests

--- projects/bp-gallery/cypress/e2e/bulk-edit/descriptions.cy.ts
+++ projects/bp-gallery/cypress/e2e/bulk-edit/descriptions.cy.ts
@@ -12,6 +12,10 @@ describe('bulk edit descriptions', () => {
   });
 
   it('shows the same description as the description', () => {
+    cy.visit('/picture/4');
+    cy.contains('Yet another description');
+    cy.visit('/picture/5');
+    cy.contains('Yet another description');
     visitArchive1Pictures();
     selectPictures('4', '5');
     cy.contains('Mehrere Bilder editieren').click();
@@ -19,13 +23,26 @@ describe('bulk edit descriptions', () => {
   });
 
   it('shows different descriptions as no description', () => {
+    cy.visit('/picture/2');
+    cy.contains('Yet another description');
+    cy.visit('/picture/3');
+    cy.contains('Keine Zeit bekannt').should('not.exist');
+    cy.contains('Keine Beschreibung vorhanden');
     visitArchive1Pictures();
     selectPictures('2', '3');
     cy.contains('Mehrere Bilder editieren').click();
+    cy.find('.MuiIconButton-root [data-testid="AddIcon"]');
+    cy.contains('Keine Zeit bekannt').should('not.exist');
     cy.contains('Keine Beschreibung vorhanden');
   });
 
   it('shows only the shared descriptions', () => {
+    cy.visit('/picture/1');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!');
+    cy.visit('/picture/2');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!').should('not.exist');
     visitArchive1Pictures();
     selectPictures('1', '2');
     cy.contains('Mehrere Bilder editieren').click();
@@ -34,6 +51,14 @@ describe('bulk edit descriptions', () => {
   });
 
   it('adds and deletes descriptions while leaving exclusive descriptions as they are', () => {
+    cy.visit('/picture/1');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!');
+    cy.contains('Irgendwas cooles').should('not.exist');
+    cy.visit('/picture/2');
+    cy.contains('Yet another description');
+    cy.contains('This is a description!').should('not.exist');
+    cy.contains('Irgendwas cooles').should('not.exist');
     visitArchive1Pictures();
     selectPictures('1', '2');
     cy.contains('Mehrere Bilder editieren').click();
