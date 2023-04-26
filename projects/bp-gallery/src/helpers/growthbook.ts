436d5ef38e8210eee02ff8bc48d3fb368e9e299b
David Schroschk <mustermannmuster2@gmail.com>
2023-04-26 09:34:42 +0200

    Add ts wrappers for important growthbook calls

---
+++ projects/bp-gallery/src/helpers/growthbook.ts
@@ -0,0 +1,12 @@
+import { GrowthBook, useFeatureIsOn as _useFeatureIsOn } from '@growthbook/growthbook-react';
+import { useGrowthBook as _useGrowthBook } from '@growthbook/growthbook-react';
+
+export type AppFeatures = {
+  test_button_navbar: boolean;
+};
+
+export const useGrowthBook = (): GrowthBook<AppFeatures> | undefined =>
+  _useGrowthBook<AppFeatures>();
+
+export const useFeatureIsOn = (id: keyof AppFeatures & string): boolean =>
+  _useFeatureIsOn<AppFeatures>(id);
