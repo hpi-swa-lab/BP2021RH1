c61cf526a56706d7585338610b3165f14afb5da0
David Schroschk <mustermannmuster2@gmail.com>
2023-04-27 11:41:48 +0200

    Add additional ts wrapper

--- projects/bp-gallery/src/helpers/growthbook.ts
+++ projects/bp-gallery/src/helpers/growthbook.ts
@@ -1,12 +1,20 @@
-import { GrowthBook, useFeatureIsOn as _useFeatureIsOn } from '@growthbook/growthbook-react';
+import {
+  GrowthBook,
+  useFeatureIsOn as _useFeatureIsOn,
+  useFeatureValue as _useFeatureValue,
+} from '@growthbook/growthbook-react';
 import { useGrowthBook as _useGrowthBook } from '@growthbook/growthbook-react';
 
 export type AppFeatures = {
-  test_button_navbar: boolean;
+  test_button: boolean;
 };
+
+type FeatureId = keyof AppFeatures & string;
 
 export const useGrowthBook = (): GrowthBook<AppFeatures> | undefined =>
   _useGrowthBook<AppFeatures>();
 
-export const useFeatureIsOn = (id: keyof AppFeatures & string): boolean =>
-  _useFeatureIsOn<AppFeatures>(id);
+export const useFeatureIsOn = (id: FeatureId): boolean => _useFeatureIsOn<AppFeatures>(id);
+
+export const useFeatureValue = ({ id, fallback }: { id: FeatureId; fallback: string }): string =>
+  _useFeatureValue(id, fallback);
