1e20de106a360b275c0192503c2ccd7dd142280c
olschulz <64468276+olschulz@users.noreply.github.com>
2023-05-09 14:33:35 +0200

    fix yarn not found error and switch deployment branch to this one in config

--- projects/bp-strapi/ecosystem.aws.config.js
+++ projects/bp-strapi/ecosystem.aws.config.js
@@ -13,23 +13,21 @@ module.exports = {
     production: {
       key: "deploy_rsa",
       user: process.env.REMOTE_USER,
-      ref: "origin/466-setup-strapi-for-aws",
+      ref: "origin/483-add-deployment-scripts-for-aws",
       host: process.env.REMOTE_HOST,
       ssh_options: "StrictHostKeyChecking=no",
       path: "/home/github/harz-history",
       repo: "https://github.com/hpi-swa-lab/BP2021RH1",
-      "post-deploy":
-        'export PATH="$PATH:~/.npm-global/bin/" && cd projects/bp-strapi && yarn install --frozen-lockfile && yarn build:prod && yarn pm2 reload ecosystem.aws.config.js --env production && yarn pm2 save',
+      "post-deploy": "cd projects/bp-strapi && ./postdeploy.sh",
     },
     staging: {
       key: "deploy.key",
       user: process.env.REMOTE_USER,
-      ref: "origin/466-setup-strapi-for-aws",
+      ref: "origin/483-add-deployment-scripts-for-aws",
       host: process.env.REMOTE_HOST,
       path: "/home/github/harz-history-staging",
       repo: "https://github.com/hpi-swa-lab/BP2021RH1",
-      "post-deploy":
-        "cd projects/bp-strapi && yarn install --frozen-lockfile && yarn build && yarn pm2 reload ecosystem.aws.config.js --env staging && yarn pm2 save",
+      "post-deploy": "cd projects/bp-strapi && ./postdeploy.staging.sh",
     },
   },
 };
