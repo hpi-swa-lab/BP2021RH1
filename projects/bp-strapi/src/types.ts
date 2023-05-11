2378099de673a19286abb696afb4ac48dcfc19d8
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-11 16:14:07 +0200

    Refactor and add scoring system and alternate pairs (umlauts + ß)

--- projects/bp-strapi/src/types.ts
+++ projects/bp-strapi/src/types.ts
@@ -1,8 +1,10 @@
-import type * as Nexus from "nexus";
 import { Strapi } from "@strapi/strapi";
 import { knex } from "knex";
+import type * as Nexus from "nexus";
 
 export type KnexEngine = typeof knex & ReturnType<typeof knex>;
+
+export type QueryBuilder = ReturnType<KnexEngine["select"]>;
 
 export type GqlExtension = {
   strapi: Strapi & { db: { connection: KnexEngine } };
