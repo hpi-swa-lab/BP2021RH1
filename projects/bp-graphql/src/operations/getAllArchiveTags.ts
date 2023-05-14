31805982c4d4060f977a5ba1301a67e28ffbd4ec
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2023-05-15 00:13:42 +0200

    add providers to queries

--- projects/bp-graphql/src/operations/getAllArchiveTags.ts
+++ projects/bp-graphql/src/operations/getAllArchiveTags.ts
@@ -18,6 +18,7 @@ export default {
                       attributes {
                         url
                         updatedAt
+                        provider
                       }
                     }
                   }
