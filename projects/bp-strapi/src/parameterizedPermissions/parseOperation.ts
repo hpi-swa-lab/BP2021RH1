e667f2d684bcb3d1562a2c0ea77752bd1d90f882
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-15 19:22:44 +0200

    Add parseOperationSource

--- projects/bp-strapi/src/parameterizedPermissions/parseOperation.ts
+++ projects/bp-strapi/src/parameterizedPermissions/parseOperation.ts
@@ -1,16 +1,21 @@
 import { Operation } from "bp-graphql/build";
 import { parse } from "graphql/language";
 
-export const parseOperation = (operation: Operation) => {
-  const document = parse(operation.document.source);
+export const parseOperationSource = (source: string, name: string) => {
+  const document = parse(source);
   if (document.definitions.length !== 1) {
-    throw new Error(
-      `operation ${operation.document.name} contains more than one definition`
-    );
+    throw new Error(`operation ${name} contains more than one definition`);
   }
   const definition = document.definitions[0];
   if (definition.kind !== "OperationDefinition") {
-    throw new Error(`operation ${operation.document.name} is not an operation`);
+    throw new Error(`operation ${name} is not an operation`);
   }
   return definition;
+};
+
+export const parseOperation = (operation: Operation) => {
+  return parseOperationSource(
+    operation.document.source,
+    operation.document.name
+  );
 };
