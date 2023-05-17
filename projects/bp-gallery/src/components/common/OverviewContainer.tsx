c1d5ca18b26a094684b91f899b984cdd941d2907
LinoH5 <lino.hellige@student.hpi.uni-potsdam.de>
2023-05-17 13:04:22 +0200

    dont use Children anymore

--- projects/bp-gallery/src/components/common/OverviewContainer.tsx
+++ projects/bp-gallery/src/components/common/OverviewContainer.tsx
@@ -1,18 +1,17 @@
 import { IconProps, Tab, Tabs, Tooltip } from '@mui/material';
-import { Children, PropsWithChildren, ReactElement, useState } from 'react';
+import { ReactElement, useState } from 'react';
 
 const OverviewContainer = ({
-  titles,
-  icons,
-  children,
-}: PropsWithChildren<{ titles?: string[]; icons: ReactElement<IconProps>[] }>) => {
-  const childrenArray = Children.toArray(children);
+  tabs,
+}: {
+  tabs: { title: string; icon: ReactElement<IconProps>; content: ReactElement }[];
+}) => {
   const [tabIndex, setTabIndex] = useState<number>(0);
 
   return (
     <div className='overview-selection-container'>
       <div className='overview-selection-header'>
-        {titles && <h2 className='overview-selection-title'>{titles[tabIndex]}</h2>}
+        <h2 className='overview-selection-title'>{tabs[tabIndex].title}</h2>
         <Tabs
           variant='scrollable'
           allowScrollButtonsMobile
@@ -21,14 +20,14 @@ const OverviewContainer = ({
             setTabIndex(value as number);
           }}
         >
-          {icons.map((icon, index) => (
-            <Tooltip key={index} title={titles && titles.length > index ? titles[index] : ''}>
-              <Tab icon={icon} />
+          {tabs.map((tab, index) => (
+            <Tooltip key={index} title={tab.title}>
+              <Tab icon={tab.icon} />
             </Tooltip>
           ))}
         </Tabs>
       </div>
-      {childrenArray[tabIndex]}
+      {tabs[tabIndex].content}
     </div>
   );
 };
