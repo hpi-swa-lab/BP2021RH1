92ad02e45e6ea9fda1b8b13348ebbd111fcf2e91
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-19 13:14:03 +0200

    Port NavigationBar to canRunOperation

--- projects/bp-gallery/src/components/top-and-bottom-bar/NavigationBar.tsx
+++ projects/bp-gallery/src/components/top-and-bottom-bar/NavigationBar.tsx
@@ -16,61 +16,89 @@ import { Menu, MenuItem } from '@mui/material';
 import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
-import { AuthRole } from '../provider/AuthProvider';
+import {
+  useCanUseCollectionCuratingView,
+  useCanUseKeywordTagTableView,
+  useCanUseLocationTagTableView,
+  useCanUsePersonTagTableView,
+  useCanUseUnverifiedCommentsView,
+  useCanUseUploadsView,
+} from '../../hooks/can-do-hooks';
+import { useAuth } from '../../hooks/context-hooks';
 import LoginDialog from './LoginDialog';
 import './NavigationBar.scss';
-import { useAuth } from '../../hooks/context-hooks';
 
 const NavigationBar = ({ isMobile }: { isMobile?: boolean }) => {
   const { t } = useTranslation();
 
   const [openLogin, setOpenLogin] = useState<boolean>(false);
 
-  const { role, loggedIn, logout } = useAuth();
+  const { loggedIn, logout } = useAuth();
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   // When a user successfully logs in, the Dialog closes
   useEffect(() => {
     if (loggedIn) {
-    setOpenLogin(false);
+      setOpenLogin(false);
     }
   }, [loggedIn]);
 
-  const curatorItems = useMemo(() => {
+  const { canUseUploadsView } = useCanUseUploadsView();
+  const { canRun: canUseKeywordTagTableView } = useCanUseKeywordTagTableView();
+  const { canRun: canUseLocationTagTableView } = useCanUseLocationTagTableView();
+  const { canRun: canUsePersonTagTableView } = useCanUsePersonTagTableView();
+  const { canUseUnverifiedCommentsView } = useCanUseUnverifiedCommentsView();
+  const { canUseCollectionCuratingView } = useCanUseCollectionCuratingView();
+
+  const moreMenuItems = useMemo(() => {
     return [
       {
         to: '/uploads-overview',
         icon: <Upload />,
         title: t('curator.uploads'),
+        active: canUseUploadsView,
       },
       {
         to: '/tags/keywords',
         icon: <Sell />,
         title: t('pictureFields.keywords'),
+        active: canUseKeywordTagTableView,
       },
       {
         to: '/tags/locations',
         icon: <Place />,
         title: t('pictureFields.locations'),
+        active: canUseLocationTagTableView,
       },
       {
         to: '/tags/people',
         icon: <Person />,
         title: t('pictureFields.people'),
+        active: canUsePersonTagTableView,
       },
       {
         to: '/comment-overview',
         icon: <Chat />,
         title: t('common.comments'),
+        active: canUseUnverifiedCommentsView,
       },
       {
         to: '/collections-overview',
         icon: <Folder />,
         title: t('pictureFields.collections'),
+        active: canUseCollectionCuratingView,
       },
-    ];
-  }, [t]);
+    ].filter(item => item.active);
+  }, [
+    t,
+    canUseUploadsView,
+    canUseKeywordTagTableView,
+    canUseLocationTagTableView,
+    canUsePersonTagTableView,
+    canUseUnverifiedCommentsView,
+    canUseCollectionCuratingView,
+  ]);
 
   return (
     <>
@@ -93,49 +121,49 @@ const NavigationBar = ({ isMobile }: { isMobile?: boolean }) => {
             {loggedIn ? t('login.logout') : t('login.title')}
           </span>
         </div>
-        {role === AuthRole.CURATOR && (
-          <div className='nav-element' onClick={event => setAnchorEl(event.currentTarget)}>
-            <MenuIcon />
-            <span className='nav-element-title'>{t('common.more')}</span>
-          </div>
-        )}
-        {role === AuthRole.CURATOR && (
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={() => setAnchorEl(null)}
-            onClick={() => setAnchorEl(null)}
-            transformOrigin={
-              isMobile
-                ? {
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                  }
-                : {
-                    vertical: 'top',
-                    horizontal: 'center',
-                  }
-            }
-            anchorOrigin={
-              isMobile
-                ? {
-                    vertical: 'top',
-                    horizontal: 'center',
-                  }
-                : {
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                  }
-            }
-            className='nav-more-menu'
-          >
-            {curatorItems.map(item => (
-              <MenuItem component={NavLink} key={item.to} to={item.to}>
-                {item.icon}
-                <span className='nav-element-title'>{item.title}</span>
-              </MenuItem>
-            ))}
-          </Menu>
+        {moreMenuItems.length > 0 && (
+          <>
+            <div className='nav-element' onClick={event => setAnchorEl(event.currentTarget)}>
+              <MenuIcon />
+              <span className='nav-element-title'>{t('common.more')}</span>
+            </div>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={() => setAnchorEl(null)}
+              onClick={() => setAnchorEl(null)}
+              transformOrigin={
+                isMobile
+                  ? {
+                      vertical: 'bottom',
+                      horizontal: 'center',
+                    }
+                  : {
+                      vertical: 'top',
+                      horizontal: 'center',
+                    }
+              }
+              anchorOrigin={
+                isMobile
+                  ? {
+                      vertical: 'top',
+                      horizontal: 'center',
+                    }
+                  : {
+                      vertical: 'bottom',
+                      horizontal: 'center',
+                    }
+              }
+              className='nav-more-menu'
+            >
+              {moreMenuItems.map(item => (
+                <MenuItem component={NavLink} key={item.to} to={item.to}>
+                  {item.icon}
+                  <span className='nav-element-title'>{item.title}</span>
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
         )}
       </div>
       <LoginDialog open={openLogin} onClose={() => setOpenLogin(false)} />
