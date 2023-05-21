e71006636d50645ad781215a824e398942daeb2e
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-21 20:35:32 +0200

    Use userId instead of username in useAuthChangeEffect

--- projects/bp-gallery/src/hooks/auth-change-effect.hook.ts
+++ projects/bp-gallery/src/hooks/auth-change-effect.hook.ts
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import { useAuth } from './context-hooks';
 
 export const useAuthChangeEffect = (callback: () => void) => {
-  const auth = useAuth();
+  const { userId } = useAuth();
   useEffect(() => {
     callback();
-  }, [auth.username, callback]);
+  }, [userId, callback]);
 };
