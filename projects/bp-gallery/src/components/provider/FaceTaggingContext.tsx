7b65836e63c882fa59926d268f35d15f27932b76
baerlach-git <83013270+baerlach-git@users.noreply.github.com>
2023-05-22 19:10:14 +0200

    the tagdirection of a tag should now be remembered while moving the tag (#519)
    * made it so that the tagdirection of a tag is remembered while moving the tag
    
    * the active tag direction is now saved in the activeTagData in the provider and not within the facetag component

--- projects/bp-gallery/src/components/provider/FaceTaggingContext.tsx
+++ projects/bp-gallery/src/components/provider/FaceTaggingContext.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, createContext } from 'react';
-import { FaceTagData } from '../views/picture/face-tagging/FaceTagTypes';
+import { FaceTagData, TagDirection } from '../views/picture/face-tagging/FaceTagTypes';
 import { Rect } from '../views/picture/face-tagging/helpers/image-rect';
 
 export type FaceTagging = {
@@ -13,6 +13,8 @@ export type FaceTagging = {
   setIsFaceTagging: Dispatch<SetStateAction<boolean>>;
   tagDirectionReferenceTagId: string | null;
   setTagDirectionReferenceTagId: Dispatch<SetStateAction<string | null>>;
+  activeTagDirection: TagDirection | null;
+  setActiveTagDirection: Dispatch<SetStateAction<TagDirection | null>>;
   imageRect: Rect | null;
 };
 
