e5de55ab8f6b07939f8257d1ae3548fdf31a97b9
ole1711 <42774821+ole1711@users.noreply.github.com>
2023-05-24 18:02:10 +0200

    remove playwright from bp-strapi

--- projects/bp-strapi/tests/selection.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-import { login } from './utils/login';
-
-test.fixme("login", async ({page}) => {
-    login;
-    await expect(page.getByRole("button", {name: "Logout"})).toHaveCount(1);
-})
