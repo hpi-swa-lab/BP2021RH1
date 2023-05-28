bd5b7870e7ea03d05ab447dbfb9bc1bc222f9fa4
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-28 13:24:38 +0200

    Notify other instances in useStorageState

--- projects/bp-gallery/src/hooks/storage-state.hook.ts
+++ projects/bp-gallery/src/hooks/storage-state.hook.ts
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useStorageState = <T extends object | string | number>(
   initialValue: T | (() => T),
   key: string,
   storage: Storage
 ) => {
-  const [value, setValue] = useState<T>(() => {
+  const [value, rawSetValue] = useState<T>(() => {
     const valueInStorage = storage.getItem(key);
     return valueInStorage
       ? (JSON.parse(valueInStorage) as T)
@@ -14,7 +14,30 @@ const useStorageState = <T extends object | string | number>(
       : initialValue;
   });
 
-  useEffect(() => storage.setItem(key, JSON.stringify(value)), [storage, key, value]);
+  const setValue = useCallback(
+    (value: T | ((currentValue: T) => T)) => {
+      rawSetValue(oldValue => {
+        const newValue = typeof value === 'function' ? value(oldValue) : value;
+
+        const oldStringifiedValue = storage.getItem(key);
+        const newStringifiedValue = JSON.stringify(newValue);
+        storage.setItem(key, newStringifiedValue);
+
+        // notify other instances of the same hook
+        window.dispatchEvent(
+          new StorageEvent('storage', {
+            key,
+            oldValue: oldStringifiedValue,
+            newValue: newStringifiedValue,
+            storageArea: storage,
+          })
+        );
+
+        return newValue;
+      });
+    },
+    [storage, key]
+  );
 
   useEffect(() => {
     window.addEventListener('storage', event => {
@@ -28,7 +51,9 @@ const useStorageState = <T extends object | string | number>(
         if (event.newValue === stringifiedValue) {
           return;
         }
-        setValue(JSON.parse(event.newValue) as T);
+        // use rawSetValue instead of setValue because we don't want
+        // to notify others again - they receive the current event, too
+        rawSetValue(JSON.parse(event.newValue) as T);
       }
     });
   }, [storage, key, value]);
