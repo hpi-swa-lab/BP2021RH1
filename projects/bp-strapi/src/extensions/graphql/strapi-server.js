127b3433ce2edc30522576af41063cf46aec3cd8
Marius Dörbandt <github.marius@doerbandt.de>
2023-05-31 18:33:04 +0200

    Move authStrategy registration to more appropriate extension

--- projects/bp-strapi/src/extensions/graphql/strapi-server.js
+++
@@ -1,8 +0,0 @@
-const {
-  authStrategy,
-} = require("../../parameterizedPermissions/authStrategy.js");
-
-module.exports = (plugin) => {
-  strapi.container.get("auth").register("content-api", authStrategy);
-  return plugin;
-};
--- projects/bp-strapi/src/extensions/users-permissions/strapi-server.ts
+++ projects/bp-strapi/src/extensions/users-permissions/strapi-server.ts
@@ -1,9 +1,14 @@
+import { registerParameterizedPermissionsAuthStrategy } from './registerParameterizedPermissionsAuthStrategy';
 import { enablePasswordTokenExpiration } from './resetPasswordTokenExpiration';
 
 export default plugin => {
   return {
     ...plugin,
     async register(...args) {
+      // before users-permissions registers its own authStrategy,
+      // so we have priority in strapi.auth.authenticate
+      registerParameterizedPermissionsAuthStrategy();
+
       await plugin.register(...args);
 
       enablePasswordTokenExpiration(plugin.config);
