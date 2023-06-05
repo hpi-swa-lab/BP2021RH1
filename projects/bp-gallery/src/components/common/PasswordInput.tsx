851a86fb85160f115baa6fb710de317314557b75
Marius Dörbandt <github.marius@doerbandt.de>
2023-06-05 15:31:47 +0200

    Hide password on blur

--- projects/bp-gallery/src/components/common/PasswordInput.tsx
+++ projects/bp-gallery/src/components/common/PasswordInput.tsx
@@ -1,6 +1,6 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { IconButton, TextField, TextFieldProps } from '@mui/material';
-import { useCallback, useState } from 'react';
+import { FocusEvent, useCallback, useRef, useState } from 'react';
 
 export const PasswordInput = (props: TextFieldProps) => {
   const [passwordShown, setPasswordShown] = useState(false);
@@ -9,13 +9,25 @@ export const PasswordInput = (props: TextFieldProps) => {
     setPasswordShown(passwordShown => !passwordShown);
   }, []);
 
+  const input = useRef<HTMLDivElement>(null);
+
+  const hidePassword = useCallback((event: FocusEvent<HTMLElement>) => {
+    const newFocus = event.relatedTarget;
+    if (input.current?.contains(newFocus)) {
+      return;
+    }
+    setPasswordShown(false);
+  }, []);
+
   return (
     <TextField
+      ref={input}
       type={passwordShown ? 'text' : 'password'}
       variant='outlined'
+      onBlur={hidePassword}
       InputProps={{
         endAdornment: (
-          <IconButton onClick={toggleShow}>
+          <IconButton onBlur={hidePassword} onClick={toggleShow}>
             {passwordShown ? <Visibility /> : <VisibilityOff />}
           </IconButton>
         ),
