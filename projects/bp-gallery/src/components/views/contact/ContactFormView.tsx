13d617ee0fab2a5dcc77a6eb3e4c6d83b3247f22
olschulz <64468276+olschulz@users.noreply.github.com>
2023-06-06 15:57:14 +0200

    adjust contact page design by using mui components

--- projects/bp-gallery/src/components/views/contact/ContactFormView.tsx
+++ projects/bp-gallery/src/components/views/contact/ContactFormView.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@mui/material';
+import { MenuItem, Select, TextField } from '@mui/material';
 import { FormEvent, useCallback, useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { asApiPath } from '../../../helpers/app-helpers';
 import { useOnChangeSetter } from '../../../hooks/onchange-setter.hook';
+import PrimaryButton from '../../common/PrimaryButton';
 import { AlertContext, AlertType } from '../../provider/AlertProvider';
 
 const ContactFormView = () => {
@@ -45,75 +46,47 @@ const ContactFormView = () => {
   );
 
   return (
-    <div className='contact-form-container flex flex-col flex-nowrap items-center m-auto p-4'>
-      <h1 className='pt-8'>{t('contact-form.title')}</h1>
-      <form onSubmit={onSubmit} className='contact-form w-fit h-fit'>
-        <div className='form-contents'>
-          <p>
-            <label className='flex flex-col flex-nowrap text-xl p-0'>
-              {t('contact-form.choose-archive-label')}
-              <select
-                onChange={useOnChangeSetter(setRecipient)}
-                value={recipient}
-                className='max-w h-6'
-              >
-                <option value='Test'>Test</option>
-                <option value='Herbert-Ahrens-Archiv'>Herbert-Ahrens-Archiv</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label className='flex flex-col flex-nowrap text-xl p-0'>
-              {t('contact-form.name-label')}
-              <input
-                onChange={useOnChangeSetter(setSenderName)}
-                value={senderName}
-                className='form-input h-5 w-80 name-input'
-                type='text'
-              />
-            </label>
-          </p>
-          <p>
-            <label className='flex flex-col flex-nowrap text-xl p-0'>
-              {t('contact-form.email-label')}
-              <input
-                onChange={useOnChangeSetter(setReplyEmail)}
-                value={replyEmail}
-                className='form-input h-5 w-80 email-input'
-                type='email'
-              />
-            </label>
-          </p>
-          <p>
-            <label className='flex flex-col flex-nowrap text-xl p-0'>
-              {t('contact-form.subject-label')}
-              <input
-                onChange={useOnChangeSetter(setSubject)}
-                value={subject}
-                className='form-input h-5 w-80 subject-input'
-              />
-            </label>
-          </p>
-          <p>
-            <label className='flex flex-col flex-nowrap text-xl p-0'>
-              {t('contact-form.message-label')}
-              <textarea
-                onChange={useOnChangeSetter(setMessage)}
-                value={message}
-                className='form-input max-w message-input h-20'
-              />
-            </label>
-          </p>
-          <div className='submit-button-container '></div>
-          <Button
-            className='submit-input'
-            sx={{ 'background-color': '#7e241d', color: 'white' }}
-            type='submit'
-          >
+    <div className='max-w-[1200px] h-full flex flex-col flex-nowrap items-center m-auto bg-white shadow-lg'>
+      <div className='mx-auto mt-20 p-4'>
+        <h1 className='pt-8'>{t('contact-form.title')}</h1>
+        <form onSubmit={onSubmit} className='flex flex-col w-96'>
+          <label className='flex flex-col flex-nowrap text-xl p-0'>
+            <p className='mb-1'>{t('contact-form.choose-archive-label')}</p>
+            <Select onChange={useOnChangeSetter(setRecipient)} value={recipient} className='max-w'>
+              {/* <MenuItem value='Test'>Test</MenuItem> */}
+              <MenuItem value='Herbert-Ahrens-Archiv'>Herbert-Ahrens-Archiv</MenuItem>
+            </Select>
+          </label>
+          <label className='flex flex-col flex-nowrap text-xl p-0'>
+            <p className='mb-1'>{t('contact-form.name-label')}</p>
+            <TextField onChange={useOnChangeSetter(setSenderName)} value={senderName} type='text' />
+          </label>
+          <label className='flex flex-col flex-nowrap text-xl p-0'>
+            <p className='mb-1'>{t('contact-form.email-label')}</p>
+            <TextField
+              onChange={useOnChangeSetter(setReplyEmail)}
+              value={replyEmail}
+              type='email'
+            />
+          </label>
+          <label className='flex flex-col flex-nowrap text-xl p-0 h-full'>
+            <p className='mb-1'>{t('contact-form.subject-label')}</p>
+            <TextField onChange={useOnChangeSetter(setSubject)} value={subject} />
+          </label>
+          <label className='flex flex-col flex-nowrap text-xl p-0 mb-5'>
+            <p className='mb-1'>{t('contact-form.message-label')}</p>
+            <TextField
+              multiline
+              onChange={useOnChangeSetter(setMessage)}
+              minRows={3}
+              value={message}
+            />
+          </label>
+          <PrimaryButton type='submit' className='!w-full'>
             {t('contact-form.submit-button-label').toString()}
-          </Button>
-        </div>
-      </form>
+          </PrimaryButton>
+        </form>
+      </div>
     </div>
   );
 };
