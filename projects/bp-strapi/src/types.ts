5d1241e1ac3626f103248ccb7c3ae971af5e7b26
Oliver Schulz <64468276+olschulz@users.noreply.github.com>
2023-06-14 15:35:54 +0200

    Strapi typing (#548)
    * add strapi entityService typings
    
    * convert to ts strict mode with lots of autogenerated anys
    
    * Improve Response types a bit

--- projects/bp-strapi/src/types.ts
+++ projects/bp-strapi/src/types.ts
@@ -1,10 +1,134 @@
-import type * as Nexus from "nexus";
-import { Strapi } from "@strapi/strapi";
+import {
+  Attribute,
+  GetAttributeValue,
+  GetAttributes,
+  Strapi,
+} from "@strapi/strapi";
 import { knex } from "knex";
+import type * as Nexus from "nexus";
 
 export type KnexEngine = typeof knex & ReturnType<typeof knex>;
 
+export type QueryBuilder = ReturnType<KnexEngine["select"]>;
+
 export type GqlExtension = {
-  strapi: Strapi & { db: { connection: KnexEngine } };
+  strapi: StrapiExtended;
   nexus: typeof Nexus;
 };
+
+export type StrapiExtended = Omit<Strapi, "entityService"> & {
+  db: { connection: KnexEngine };
+  entityService: EntityService;
+};
+
+type FilterParameters = Partial<
+  Record<
+    | "$and"
+    | "$or"
+    | "$not"
+    | "$eq"
+    | "$eqi"
+    | "$ne"
+    | "$in"
+    | "$notIn"
+    | "$lt"
+    | "$lte"
+    | "$gt"
+    | "$gte"
+    | "$between"
+    | "$contains"
+    | "$notContains"
+    | "$containsi"
+    | "$notContainsi"
+    | "$startsWith"
+    | "$endsWith"
+    | "$null"
+    | "$notNull",
+    any
+  >
+>;
+
+type OrderByParameter =
+  | string
+  | { [key: string]: string }
+  | { [key: string]: string }[];
+
+type ResponseAttributes<T extends Strapi.ContentTypeUIDs> =
+  | null
+  | (Partial<{
+      [K in keyof GetAttributes<T>]: DefaultType<T, K>;
+    }> & { id: number });
+
+type PopulateType<T extends Strapi.ContentTypeUIDs> = {
+  [K in keyof GetAttributes<T> as GetAttributes<T>[K] extends Attribute<"relation">
+    ? K
+    : never]: GetAttributes<T>[K];
+};
+
+type PopulateParameter<T extends Strapi.ContentTypeUIDs> =
+  | "*"
+  | (keyof PopulateType<T>)[]
+  | { [key: string]: boolean }
+  | {
+      [key: string]: {
+        fields: string[];
+        filters: FilterParameters;
+        sort: string;
+        populate: PopulateParameter<T>;
+      };
+    };
+
+type PublicationStateParameter = "live" | "preview";
+
+type Params<T extends Strapi.ContentTypeUIDs> = Partial<{
+  fields: (keyof GetAttributes<T>)[];
+  populate: PopulateParameter<T>;
+}>;
+
+type FindManyParams<T extends Strapi.ContentTypeUIDs> = Partial<
+  Params<T> & {
+    filters: FilterParameters;
+    start: Number;
+    limit: Number;
+    sort: OrderByParameter;
+    publicationState: PublicationStateParameter;
+  }
+>;
+
+type DefaultType<
+  T extends Strapi.ContentTypeUIDs,
+  L extends keyof GetAttributes<T>
+> = GetAttributes<T>[L] extends Attribute
+  ? GetAttributeValue<GetAttributes<T>[L]> extends boolean
+    ? boolean
+    : GetAttributeValue<GetAttributes<T>[L]>
+  : any;
+
+type MutateParams<T extends Strapi.ContentTypeUIDs> = Params<T> & {
+  data: Partial<{ [K in keyof GetAttributes<T>]: DefaultType<T, K> }>;
+};
+
+export interface EntityService {
+  findOne: <T extends Strapi.ContentTypeUIDs>(
+    uid: T,
+    id: number,
+    parameters: Params<T>
+  ) => Promise<ResponseAttributes<T>>;
+  findMany: <T extends Strapi.ContentTypeUIDs, K extends FindManyParams<T>>(
+    uid: T,
+    parameters: K
+  ) => Promise<ResponseAttributes<T>[]>;
+  create: <T extends Strapi.ContentTypeUIDs>(
+    uid: T,
+    parameters: Params<T> & { data: Object }
+  ) => Promise<Object>;
+  update: <T extends Strapi.ContentTypeUIDs>(
+    uid: T,
+    parameters: MutateParams<T>
+  ) => Promise<Object>;
+  delete: <T extends Strapi.ContentTypeUIDs>(
+    uid: T,
+    id: number,
+    parameters: Params<T>
+  ) => Promise<Object>;
+}
