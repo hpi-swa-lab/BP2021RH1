5eb9b7ce2712d7a54e016bbf6bd8ed742eb4eba1
MariusDoe <41512063+MariusDoe@users.noreply.github.com>
2023-06-14 10:33:29 +0200

    Remove prototypes (#549)

--- projects/bp-gallery/src/components/routes.tsx
+++ projects/bp-gallery/src/components/routes.tsx
@@ -1,26 +1,24 @@
 import { RouteConfig, RouteConfigComponentProps } from 'react-router-config';
 import { Redirect } from 'react-router-dom';
-import Demo from '../prototypes/demo';
-import TimeLineDemo from '../prototypes/timeline-demo';
-import CollectionCuratingView from './views/collection-curating/CollectionCuratingView';
-import TagTableView from './views/tag-table/TagTableView';
-import PictureView from './views/picture/PictureView';
-import UnverifiedCommentsView from './views/unverified-comments/UnverifiedCommentsView';
 import { TagType } from '../types/additionalFlatTypes';
-import BrowseView from './views/browse/BrowseView';
-import SearchView from './views/search/SearchView';
-import UploadsView from './views/uploads/UploadsView';
-import LatestPicturesView from './views/latest-pictures/LatestPicturesView';
-import BulkEditView from './views/bulk-edit/BulkEditView';
-import StartView from './views/start/StartView';
-import ArchiveView from './views/archives/ArchiveView';
-import ArchiveEditView from './views/archives/ArchiveEditView';
 import ProtectedRoute from './common/ProtectedRoute';
-import TermsOfServiceView from './views/terms-of-service/TermsOfServiceView';
+import ArchiveEditView from './views/archives/ArchiveEditView';
+import ArchiveView from './views/archives/ArchiveView';
+import BrowseView from './views/browse/BrowseView';
+import BulkEditView from './views/bulk-edit/BulkEditView';
+import CollectionCuratingView from './views/collection-curating/CollectionCuratingView';
 import ContactFormView from './views/contact/ContactFormView';
-import ShowMoreView from './views/show-more/ShowMoreView';
 import DiscoverView from './views/discover/DiscoverView';
 import GeoView from './views/geoguessr/GeoView';
+import LatestPicturesView from './views/latest-pictures/LatestPicturesView';
+import PictureView from './views/picture/PictureView';
+import SearchView from './views/search/SearchView';
+import ShowMoreView from './views/show-more/ShowMoreView';
+import StartView from './views/start/StartView';
+import TagTableView from './views/tag-table/TagTableView';
+import TermsOfServiceView from './views/terms-of-service/TermsOfServiceView';
+import UnverifiedCommentsView from './views/unverified-comments/UnverifiedCommentsView';
+import UploadsView from './views/uploads/UploadsView';
 
 export const FALLBACK_PATH = '/start';
 
@@ -119,14 +117,6 @@ const routes: RouteConfig[] = [
     render: () => {
       return <ContactFormView />;
     },
-  },
-  {
-    path: '/prototypes/demo',
-    component: Demo,
-  },
-  {
-    path: '/prototypes/timeline-demo',
-    component: TimeLineDemo,
   },
   {
     path: '/archives/:id/edit',
