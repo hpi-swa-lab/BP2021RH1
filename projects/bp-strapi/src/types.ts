01b5e1721d5c3b3f2957a1c26a20718102d1eb41
olschulz <64468276+olschulz@users.noreply.github.com>
2023-06-14 17:32:36 +0200

    update context type and use it for growthbook middleware

--- projects/bp-strapi/src/types.ts
+++ projects/bp-strapi/src/types.ts
@@ -1,11 +1,11 @@
-import {
+import type {
   Attribute,
   GetAttributeValue,
   GetAttributes,
   Strapi,
 } from "@strapi/strapi";
-import { knex } from "knex";
-import { Context } from "koa";
+import type { knex } from "knex";
+import type { ParameterizedContext } from "koa";
 import type * as Nexus from "nexus";
 
 export type KnexEngine = typeof knex & ReturnType<typeof knex>;
@@ -17,21 +17,34 @@ export type GqlExtension = {
   nexus: typeof Nexus;
 };
 
+type ContextState = {
+  withGrowthBook?: (theCtx: StrapiGqlContext, f: any) => void;
+};
+
 // See https://koajs.com/#context and https://github.com/jeffijoe/koa-respond
-export type StrapiContext = Context & {
+type Context = {
   send: (statusCode: number, body: string | Object) => StrapiContext;
+  req?: { headers?: { anonymousid?: string } };
 } & Record<
-    | "created"
-    | "noContent"
-    | "badRequest"
-    | "unauthorized"
-    | "forbidden"
-    | "notFound"
-    | "locked"
-    | "internalServerError"
-    | "notImplemented",
-    (body: string | Object) => StrapiContext
-  >;
+  | "created"
+  | "noContent"
+  | "badRequest"
+  | "unauthorized"
+  | "forbidden"
+  | "notFound"
+  | "locked"
+  | "internalServerError"
+  | "notImplemented",
+  (body: string | Object) => StrapiContext
+>;
+
+export type StrapiContext = ParameterizedContext<ContextState, Context>;
+
+export type StrapiGqlContext = {
+  state: ContextState;
+  koaContext: StrapiContext;
+};
+
 export type StrapiExtended = Omit<Strapi, "entityService"> & {
   db: { connection: KnexEngine };
   entityService: EntityService;
