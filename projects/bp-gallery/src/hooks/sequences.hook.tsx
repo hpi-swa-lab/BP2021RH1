727a2fd36e50aaccbceefe8da9a1ca0622d62a66
Marius Dörbandt <github.marius@doerbandt.de>
2023-06-16 17:59:07 +0200

    Add option to join sequences

--- projects/bp-gallery/src/hooks/sequences.hook.tsx
+++ projects/bp-gallery/src/hooks/sequences.hook.tsx
@@ -1,12 +1,13 @@
-import { unionWith } from 'lodash';
+import { Add, Close, Merge } from '@mui/icons-material';
+import { unionWith, uniqBy } from 'lodash';
 import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { DialogPreset, useDialog } from '../components/provider/DialogProvider';
+import { useDialog } from '../components/provider/DialogProvider';
 import {
   useCreatePictureSequenceMutation,
   useUpdatePictureMutation,
 } from '../graphql/APIConnector';
-import { FlatPicture } from '../types/additionalFlatTypes';
+import { FlatPicture, FlatPictureSequence } from '../types/additionalFlatTypes';
 
 export const useCreateSequence = () => {
   const { t } = useTranslation();
@@ -20,14 +21,47 @@ export const useCreateSequence = () => {
 
   const createSequence = useCallback(
     async (pictures: FlatPicture[]) => {
-      if (pictures.some(picture => !!picture.picture_sequence)) {
-        const really = await dialog({
-          preset: DialogPreset.CONFIRM,
+      const existingSequence = pictures.find(
+        picture => !!picture.picture_sequence
+      )?.picture_sequence;
+      if (existingSequence) {
+        enum Options {
+          ABORT,
+          JOIN_ALL_SEQUENCES,
+          NEW_SEQUENCE,
+        }
+        const option = await dialog({
           title: t('curator.alreadyInSequence.title'),
           content: t('curator.alreadyInSequence.content'),
+          options: [
+            { name: t('common.abort'), icon: <Close />, value: Options.ABORT },
+            {
+              name: t('curator.alreadyInSequence.joinAllSequences'),
+              icon: <Merge />,
+              value: Options.JOIN_ALL_SEQUENCES,
+            },
+            {
+              name: t('curator.alreadyInSequence.newSequence'),
+              icon: <Add />,
+              value: Options.NEW_SEQUENCE,
+            },
+          ],
         });
-        if (!really) {
-          return;
+        switch (option) {
+          case Options.NEW_SEQUENCE:
+            // fall through to the createPictureSequence call
+            break;
+          case Options.JOIN_ALL_SEQUENCES: {
+            const allPictures = uniqBy(
+              pictures.flatMap(picture => picture.picture_sequence?.pictures ?? [picture]),
+              picture => picture.id
+            );
+            updatePictureSequenceOrder(allPictures, existingSequence);
+            return;
+          }
+          case Options.ABORT:
+          default:
+            return;
         }
       }
       const pictureSequence = await createPictureSequence({
@@ -46,11 +80,11 @@ export const useCreateSequence = () => {
 
 export const useUpdatePictureSequenceOrder = () => {
   const [updatePicture] = useUpdatePictureMutation({
-    refetchQueries: ['getPictureInfo'],
+    refetchQueries: ['getPictureInfo', 'getPictures', 'getMostLikedPictures'],
   });
 
   const updatePictureSequenceOrder = useCallback(
-    async (pictures: FlatPicture[]) => {
+    async (pictures: FlatPicture[], sequence?: FlatPictureSequence) => {
       await Promise.all(
         pictures.map((picture, index) =>
           updatePicture({
@@ -58,6 +92,7 @@ export const useUpdatePictureSequenceOrder = () => {
               pictureId: picture.id,
               data: {
                 picture_sequence_order: index + 1, // avoid 0
+                picture_sequence: sequence?.id,
               },
             },
           })
