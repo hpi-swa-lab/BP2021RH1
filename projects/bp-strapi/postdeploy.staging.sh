de95e817fd89834b58cbcb1fc25de6d65e7551e2
MariusDoe <41512063+MariusDoe@users.noreply.github.com>
2023-06-26 15:54:40 +0200

    582 fix postdeploy (#583)
    * Add line breaks to postdeploy scripts
    
    * Return early on error
    
    * Build bp-graphql before installing it

--- projects/bp-strapi/postdeploy.staging.sh
+++ projects/bp-strapi/postdeploy.staging.sh
@@ -1,1 +1,7 @@
-export PATH="$PATH:~/.nvm/versions/node/v18.16.0/bin/" && yarn install --frozen-lockfile && yarn build && yarn pm2 reload ecosystem.aws.config.js --env staging && yarn pm2 save
+set -e
+export PATH="$PATH:~/.nvm/versions/node/v18.16.0/bin/"
+(cd ../bp-graphql && yarn install --frozen-lockfile && tsc)
+yarn install --frozen-lockfile
+yarn build
+yarn pm2 reload ecosystem.aws.config.js --env staging
+yarn pm2 save
