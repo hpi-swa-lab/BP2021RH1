ad14da2d9a3ac235e1840100eb42e9cf7ac2e065
MariusDoe <41512063+MariusDoe@users.noreply.github.com>
2023-06-26 17:29:21 +0200

    Add yarn upgrade bp-graphql (#585)

--- projects/bp-strapi/postdeploy.staging.sh
+++ projects/bp-strapi/postdeploy.staging.sh
@@ -2,6 +2,7 @@ set -e
 export PATH="$PATH:~/.nvm/versions/node/v18.16.0/bin/"
 (cd ../bp-graphql && yarn install --frozen-lockfile && yarn build)
 yarn install --frozen-lockfile
+yarn upgrade bp-graphql
 yarn build
 yarn pm2 reload ecosystem.aws.config.js --env staging
 yarn pm2 save
