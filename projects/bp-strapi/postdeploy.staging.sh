019586bfba367d3071987ed62548ca37d1a826c8
Marius Dörbandt <github.marius@doerbandt.de>
2023-06-26 15:33:17 +0200

    Build bp-graphql before installing it

--- projects/bp-strapi/postdeploy.staging.sh
+++ projects/bp-strapi/postdeploy.staging.sh
@@ -1,5 +1,6 @@
 set -e
 export PATH="$PATH:~/.nvm/versions/node/v18.16.0/bin/"
+(cd ../bp-graphql && yarn install --frozen-lockfile && tsc)
 yarn install --frozen-lockfile
 yarn build
 yarn pm2 reload ecosystem.aws.config.js --env staging
