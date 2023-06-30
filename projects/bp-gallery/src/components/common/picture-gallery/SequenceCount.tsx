0dfcb3298ad6d38fa86b0e10aa1a5b32ebdc20f9
Marius Dörbandt <github.marius@doerbandt.de>
2023-06-30 13:26:36 +0200

    Move icons array out of component

--- projects/bp-gallery/src/components/common/picture-gallery/SequenceCount.tsx
+++ projects/bp-gallery/src/components/common/picture-gallery/SequenceCount.tsx
@@ -12,20 +12,21 @@ import {
   FilterNone,
 } from '@mui/icons-material';
 
+const icons = [
+  FilterNone,
+  Filter1,
+  Filter2,
+  Filter3,
+  Filter4,
+  Filter5,
+  Filter6,
+  Filter7,
+  Filter8,
+  Filter9,
+  Filter9Plus,
+];
+
 export const SequenceCount = ({ count }: { count: number }) => {
-  const icons = [
-    FilterNone,
-    Filter1,
-    Filter2,
-    Filter3,
-    Filter4,
-    Filter5,
-    Filter6,
-    Filter7,
-    Filter8,
-    Filter9,
-    Filter9Plus,
-  ];
   const Icon = icons[Math.min(count, icons.length - 1)];
   return <Icon />;
 };
