f4dd938a8711bd8d34f2eea3f7535c928d6193ad
Marius Dörbandt <github.marius@doerbandt.de>
2023-07-07 15:57:22 +0200

    Add noDistractionModeStyle to PresentationModeButton

--- projects/bp-gallery/src/components/views/picture/overlay/PresentationModeButton.tsx
+++ projects/bp-gallery/src/components/views/picture/overlay/PresentationModeButton.tsx
@@ -1,6 +1,7 @@
 import { PresentToAll } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { useNoDistractionModeStyle } from '../helpers/no-distraction-mode-style';
 
 export const PresentationModeButton = ({
   pictureId,
@@ -11,9 +12,11 @@ export const PresentationModeButton = ({
 }) => {
   const { t } = useTranslation();
 
+  const noDistractionModeStyle = useNoDistractionModeStyle();
+
   return (
     <Button
-      className='presentation-mode-button'
+      className={`presentation-mode-button ${noDistractionModeStyle}`}
       onClick={() => {
         window.open(`/picture/${pictureId}?presentation=${sessionId}`, '_blank', 'fullscreen=1');
       }}
